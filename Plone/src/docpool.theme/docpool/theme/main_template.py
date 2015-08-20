fe150fe27dd0b10ac82a3622b82065d69144aee1
ext-ke-rih-sz <devnull@localhost>
2015-08-20 13:51:57 +0200

    docpool.theme initial package

---
+++ Plone/src/docpool.theme/docpool/theme/main_template.py
@@ -0,0 +1,20 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFPlone.browser.main_template import MainTemplate as OrigMainTemplate
+
+
+class MainTemplate(OrigMainTemplate):
+
+    popup_template = ViewPageTemplateFile('templates/popup_main_template.pt')
+
+    def __call__(self):
+        return self.template()
+
+    @property
+    def template(self):
+        if self.request.form.get('popup_load'):
+            return self.popup_template
+        elif self.request.form.get('ajax_load'):
+            return OrigMainTemplate.ajax_template
+        else:
+            return OrigMainTemplate.main_template
---
+++ Plone/src/docpool.theme/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='docpool.theme',
+      version=version,
+      description="DocPool Theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['docpool'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          # -*- Extra requirements: -*-
+          'quintagroup.dropdownmenu',          
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
