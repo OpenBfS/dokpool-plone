8083a6cbb642d3813dd177919346148608f78cbb
ext-ke-rih-sz <devnull@localhost>
2015-10-14 15:38:26 +0200

    Deleted old sections viewlet

--- Plone/src/docpool.theme/docpool/theme/browser/viewlets/common.py
+++ Plone/src/docpool.theme/docpool/theme/browser/viewlets/common.py
@@ -1,16 +1,10 @@
-from plone.app.layout.viewlets.common import GlobalSectionsViewlet as GSV, ViewletBase
+from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from AccessControl.SecurityInfo import allow_module, allow_class
 
 allow_module("elan.esd.browser");
 allow_module("elan.esd.browser.viewlets");
 allow_module("elan.esd.browser.viewlets.common");
-
-class GlobalSectionsViewlet(GSV):
-    index = ViewPageTemplateFile('sections.pt')
-    
-    def update(self):
-        GSV.update(self)
         
 class TimeViewlet(ViewletBase):
     index = ViewPageTemplateFile('time.pt')
