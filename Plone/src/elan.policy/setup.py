f474e6a6e8f706d83e0f77e417315d82f485f662
Marco Lechner, Bundesamt fuer Strahlenschutz, SW 2.1 <mlechner@bfs.de>
2015-11-13 10:22:04 +0100

    changed license in elan.policy and elan.dbaccess to GPL

--- Plone/src/elan.policy/setup.py
+++ Plone/src/elan.policy/setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
 
@@ -17,7 +18,7 @@ setup(name='elan.policy',
       author='Condat AG',
       author_email='hr@condat.de',
       url='http://xservice.condat.de/vportal/elan-e',
-      license='Commercial',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['elan'],
       include_package_data=True,
