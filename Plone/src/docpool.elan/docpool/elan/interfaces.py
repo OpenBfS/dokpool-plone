1a53f45472c06c9c740b50cfadf3e8a8e87ec3a1
Henning Rietz <hr@condat.de>
2017-01-19 10:29:09 +0100

    removed unused types

--- Plone/src/docpool.elan/docpool/elan/interfaces.py
+++ Plone/src/docpool.elan/docpool/elan/interfaces.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from zope.interface import Interface, Attribute
-from docpool.elan.content.elandocument import IELANDocument
-from docpool.elan.content.elantype import IELANType
 ##code-section imports
 from docpool.elan import DocpoolMessageFactory as _
 ##/code-section imports
--- Plone/src/docpool.elan/setup.py
+++ Plone/src/docpool.elan/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0.6'
+version = '0.0.7'
 
 setup(name='docpool.elan',
       version=version,
