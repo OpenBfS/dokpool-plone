e3c71a92744bd0ef0521cc0435f1fc9116e41956
Henning Rietz <hr@condat.de>
2017-03-24 18:11:27 +0100

    https://redmine-koala.bfs.de/issues/1628

---
+++ Plone/src/docpool.video/docpool/video/monkey.py
@@ -0,0 +1,7 @@
+def getVideos(self, **kwargs):
+    """
+    """
+    args = {'portal_type': 'WildcardVideo'}
+    args.update(kwargs)
+    return [obj.getObject() for obj in self.getFolderContents(args)]
+
