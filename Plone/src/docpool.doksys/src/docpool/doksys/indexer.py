1480a50efb0d2b07af6e69131faaf45e71e03af7
Peter Bieringer <pbieringer@bfs.de>
2018-07-26 14:29:47 +0200

    added indices and adapter for indexers

--- Plone/src/docpool.doksys/src/docpool/doksys/indexer.py
+++ Plone/src/docpool.doksys/src/docpool/doksys/indexer.py
@@ -11,6 +11,28 @@ def network_operator_indexer(obj):
     except:
         pass
 
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).dom
+    except:
+        pass
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).legal_base
+    except:
+        pass
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).measuring_program
+    except:
+        pass
+
+
 
 @indexer(IDPDocument)
 def sampling_begin_indexer(obj):
@@ -18,3 +40,124 @@ def sampling_begin_indexer(obj):
         return obj.doc_extension(DOKSYS_APP).sampling_begin
     except:
         pass
+
+
+@indexer(IDPDocument)
+def sampling_begin_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).sampling_end
+    except:
+        pass
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).purpose
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).trajectory_start_location
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).trajectory_end_location
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).trajectory_start_time
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).trajectory_end_time
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).status
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).operation_mode
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).data_type
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).sample_type_id
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).sample_type
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).measurement_category
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).duration
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).type
+    except:
+        pass
+
+
+@indexer(IDPDocument)
+def network_operator_indexer(obj):
+    try:
+        return obj.doc_extension(DOKSYS_APP).area
+    except:
+        pass
+
+
