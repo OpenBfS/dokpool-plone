477d0d827fdc2fcada9b563be99274f73728aca0
Peter Bieringer <pbieringer@bfs.de>
2018-07-27 13:42:51 +0200

    added templates for all needed vocabularies

---
+++ Plone/src/docpool.doksys/src/docpool/doksys/vocabularies/legal_base.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# from plone import api
+from docpool.doksys import _
+from plone.dexterity.interfaces import IDexterityContent
+from zope.globalrequest import getRequest
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+class VocabItem(object):
+    def __init__(self, token, value):
+        self.token = token
+        self.value = value
+
+
+@implementer(IVocabularyFactory)
+class LegalBase(object):
+    """
+    """
+
+    def __call__(self, context):
+        # Just an example list of content for our vocabulary,
+        # this can be any static or dynamic data, a catalog result for example.
+        items = [
+
+        ]
+
+        # Fix context if you are using the vocabulary in DataGridField.
+        # See https://github.com/collective/collective.z3cform.datagridfield/issues/31:  # NOQA: 501
+        if not IDexterityContent.providedBy(context):
+            req = getRequest()
+            context = req.PARENTS[0]
+
+        # create a list of SimpleTerm items:
+        terms = []
+        for item in items:
+            terms.append(
+                SimpleTerm(
+                    value=item.token,
+                    token=str(item.token),
+                    title=item.value,
+                )
+            )
+        # Create a SimpleVocabulary from the terms list and return it:
+        return SimpleVocabulary(terms)
+
+
+LegalBaseFactory = LegalBase()
