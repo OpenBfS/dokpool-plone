a74bd310950500efca7ebf63c5796730ab7f1515
Philip Bauer <bauer@starzel.de>
2019-08-14 11:01:55 +0200

    re-add removed app-registration for doksys

--- Plone/src/docpool.doksys/src/docpool/doksys/__init__.py
+++ Plone/src/docpool.doksys/src/docpool/doksys/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 from AccessControl import allow_class
+from docpool.doksys import appregistration  # noqa: F401
 from zope.i18nmessageid import MessageFactory
 
 
