7c26dead870be5cd82d74142c708d179e972805c
Philip Bauer <bauer@starzel.de>
2019-08-14 14:02:04 +0200

    more appregistration import behind DocpoolMessageFactory

--- Plone/src/docpool.doksys/src/docpool/doksys/__init__.py
+++ Plone/src/docpool.doksys/src/docpool/doksys/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 from AccessControl import allow_class
-from docpool.doksys import appregistration  # noqa: F401
 from zope.i18nmessageid import MessageFactory
 
 
 _ = DocpoolMessageFactory = MessageFactory('docpool.doksys')
 allow_class(DocpoolMessageFactory)
+
+from docpool.doksys import appregistration  # noqa: F401
