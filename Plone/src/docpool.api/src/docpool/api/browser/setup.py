be039beb492e4f6682fe9648dad7c98164130394
Philip Bauer <bauer@starzel.de>
2019-09-26 16:47:14 +0200

    finish first version of docpool_setup

--- Plone/src/docpool.api/src/docpool/api/browser/setup.py
+++ Plone/src/docpool.api/src/docpool/api/browser/setup.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from plone.app.textfield import RichTextValue
 from plone.dexterity.events import EditFinishedEvent
+from plone.namedfile.file import NamedBlobImage
 from plone.protect.interfaces import IDisableCSRFProtection
+from plone.uuid.interfaces import IUUID
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import get_installer
 from Products.Five.browser import BrowserView
 from zope.component import getUtility
@@ -9,11 +13,18 @@ from zope.event import notify
 from zope.interface import alsoProvides
 from zope.lifecycleevent import modified
 from zope.schema.interfaces import IVocabularyFactory
-from plone.app.textfield import RichTextValue
 
 import logging
+import os
 
 log = logging.getLogger(__name__)
+
+
+def dummy_image(filename=u'image.png'):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    with open(filename, 'rb') as fd:
+        data = fd.read()
+    return NamedBlobImage(data=data, filename=filename)
 
 
 class DocpoolSetup(BrowserView):
@@ -28,6 +39,9 @@ class DocpoolSetup(BrowserView):
     """
 
     def __call__(self):
+        if not self.request.form.get('submit', None):
+            return self.index()
+
         alsoProvides(self.request, IDisableCSRFProtection)
 
         # install addons
@@ -37,56 +51,46 @@ class DocpoolSetup(BrowserView):
         installer.install_product('elan.journal')
 
         # TODO: pack bundles
+        # @Steffen?
 
         # create docpool 1
-        docpool = api.content.create(
+        docpool_bund = api.content.create(
             container=self.context,
             type='DocumentPool',
             id='bund',
             title=u'Bund',
+            prefix='bund',
             supportedApps=('elan', 'doksys'),
         )
-        notify(EditFinishedEvent(docpool))
+        notify(EditFinishedEvent(docpool_bund))
 
         # create docpool 2
-        docpool_2 = api.content.create(
+        docpool_land = api.content.create(
             container=self.context,
             type='DocumentPool',
-            id='bayern',
-            title=u'Bayern',
+            id='hessen',
+            title=u'Hessen',
+            prefix='hessen',
             supportedApps=('elan', 'doksys'),
         )
-        notify(EditFinishedEvent(docpool_2))
+        notify(EditFinishedEvent(docpool_land))
 
-        # add a user to test with
-        user = api.user.create(
-            email=u'tester@plone.org',
-            username=u'tester',
-            password=u'tester',
-            roles=('Member',),
-            properties={'fullname': u'Tester'},
-        )
+        user1 = self.add_user(docpool_bund, 'user1', 'group1')
+        user2 = self.add_user(docpool_land, 'user2', 'group2')
 
-        # add the user to the groups
-        docpool_contentadmins = api.group.get('bund_ContentAdministrators')
-        api.group.add_user(group=docpool_contentadmins, user=user)
-        api.group.grant_roles(
-            group=docpool_contentadmins,
-            roles=['DocPoolUser', 'DoksysUser', 'ELANUser', 'Member', 'Reader'],
-            )
+        # get the content-folder for a group to test with
+        folder = docpool_bund['content']['Groups']['bund_group1']
 
-        # enable all doctypes for this group
         voc = getUtility(IVocabularyFactory, name='docpool.base.vocabularies.DocType')
         doctypes = voc(self.context).by_value
         doctypes_ids = [i.id for i in doctypes]
-        docpool_contentadmins.setGroupProperties({'allowedDocTypes': doctypes_ids})
 
-        # get the content-folder for a group to test with
-        groups = docpool['content']['Groups']
-        folder = groups['bund_ContentAdministrators']
+        # TODO a new event
+        # folder = docpool_bund['content']['Groups']
+        event_id = 'routinemode'
 
-        # add dpdocuments of all types
-        with api.env.adopt_user(user=user):
+        # add one dpdocument for each type
+        with api.env.adopt_user(user=user1):
             for doctype_id in doctypes_ids:
                 new = api.content.create(
                     container=folder,
@@ -94,11 +98,78 @@ class DocpoolSetup(BrowserView):
                     title=u'{}'.format(doctype_id.capitalize()),
                     description=u'foo',
                     docType=doctype_id,
-                    text=RichTextValue(u'<p>Body one</p>', u'text/plain', u'text/html'),
+                    text=RichTextValue(u'<p>Body one</p>', 'text/html', 'text/x-html-safe'),
+                    local_behaviors=['elan'],
+                    scenarios=[event_id],
                 )
                 modified(new)
+                api.content.create(
+                    container=new,
+                    type='Image',
+                    title='{}_image'.format(doctype_id),
+                    image=dummy_image()
+                    )
+
                 try:
                     api.content.transition(obj=new, transition='publish')
                 except Exception as e:
                     log.info(e)
         return self.request.response.redirect(self.context.absolute_url())
+
+    def add_user(self, docpool, username, groupname):
+        """Create a User and a Group for a docpool for testing
+
+        This is mimiking the code in docpool.users
+        """
+        gtool = getToolByName(docpool, 'portal_groups')
+        docpool_uid = IUUID(docpool)
+        docpool_title = docpool.Title()
+        addname = groupname
+        group_title = groupname
+        description = ''
+        prefix = docpool.prefix or docpool.id
+        addname = "%s_%s" % (prefix, addname)
+        group_title = '{} ({})'.format(group_title, docpool.Title())
+        props = {
+            'title': group_title,
+            'description': 'A group to test with',
+            'dp': docpool_uid,
+        }
+        # this uses the monkey-patched addGroup from docpool.users
+        gtool.addGroup(
+            addname,
+            (),
+            (),
+            properties=props,
+            title=group_title,
+            description=description,
+            REQUEST=self.request,
+        )
+        # it should create some content inside teh docpool. check for it:
+        docpool.keys
+        group1 = api.group.get(addname)
+
+        # enable all doctypes for the new group
+        voc = getUtility(IVocabularyFactory, name='docpool.base.vocabularies.DocType')
+        doctypes = voc(docpool).by_value
+        doctypes_ids = [i.id for i in doctypes]
+        group1.setGroupProperties({'allowedDocTypes': doctypes_ids})
+        # set docpool on group
+        group1.setGroupProperties({'dp': docpool_uid})
+
+        # add a user to test with
+        user_fullname = '{} ({})'.format(username, docpool_title)
+        user = api.user.create(
+            email=u'tester@plone.org',
+            username=username,
+            password=username,
+            roles=('Member',),
+            properties={'fullname': user_fullname},
+        )
+        user.setMemberProperties(
+            {'fullname': user_fullname, 'dp': docpool_uid}
+        )
+        api.group.add_user(group=group1, user=user)
+        api.group.add_user(groupname='{}_DoksysUsers'.format(prefix), user=user)
+        api.group.add_user(groupname='{}_ELANUsers'.format(prefix), user=user)
+        return user
