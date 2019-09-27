b4510f838089164136e814220beeb9d429ef8e27
Steffen Lindner <mail@steffen-lindner.de>
2019-09-27 15:35:31 +0200

    Add upgrade step to update all paths of js/css

--- Plone/src/docpool.base/docpool/base/upgrades.py
+++ Plone/src/docpool.base/docpool/base/upgrades.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
-
-from plone import api
-from plone.app.upgrade.utils import loadMigrationProfile
-from plone.uuid.interfaces import IUUID
-from Products.CMFCore.utils import getToolByName
-from zope.lifecycleevent import modified
-
 import logging
-import six
 
 
 logger = logging.getLogger(__name__)
 
 def to_1_3_29(context):
-    context.runAllImportStepsFromProfile('profile-docpool:to_1_3_29')
+    context.runAllImportStepsFromProfile('profile-docpool.base:to_1_3_29')
     logger.info('Updated registry with new js/css paths')
