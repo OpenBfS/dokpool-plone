cc5b15c9334e206b354bdd32ff9418fe61e2db91
Steffen Lindner <mail@steffen-lindner.de>
2019-10-09 14:03:31 +0200

    Set generic windowName for popup overviews

--- Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
+++ Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
@@ -5,10 +5,12 @@ var thisX=0;
 
 function makePopUp(thiswidth,thisheight,thisDocument,thisWindowName,thisXPosition,thisYPosition,thisScrollbar,thisResize) {
 	myProperty = 'toolbar=0,location=0,directories=0,status=0,scrollbars=' + thisScrollbar + ',resizable=' + thisResize + ',width=' + thiswidth + ',height=' + thisheight + ',top=' + thisY + ',left=' + thisX;
-	
+	// Generates a unique window name like "Overview - Hessen" or "Overview - Bund"
+	path_names = window.location.pathname.split('/');
+	generic_window_name = thisWindowName + "-" + path_names[path_names.length -2];
 	if(self.myWindow == null){
 		delete myWindow;
-		myWindow = window.open(thisDocument,thisWindowName,myProperty);
+		myWindow = window.open(thisDocument,generic_window_name,myProperty);
 		if(myWindow != null){
 			myWindow.focus();
 		}
@@ -16,10 +18,10 @@ function makePopUp(thiswidth,thisheight,thisDocument,thisWindowName,thisXPositio
 	else{
 		if(!(myWindow.closed)){
 			self.myWindow.close();
-			myWindow  = window.open(thisDocument,thisWindowName,myProperty);
+			myWindow  = window.open(thisDocument,generic_window_name,myProperty);
 		}
 		else{
-			myWindow  = window.open(thisDocument,thisWindowName,myProperty);
+			myWindow  = window.open(thisDocument,generic_window_name,myProperty);
 		}
 	}
 }
