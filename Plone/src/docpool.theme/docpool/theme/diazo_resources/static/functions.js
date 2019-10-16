29d39f9d91fc0b2a09562d703398a8189fd7bb2e
Steffen Lindner <mail@steffen-lindner.de>
2019-10-16 12:24:03 +0200

    Remove duplicte compiled file, fix wrong closing of popups

--- Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
+++ Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
@@ -8,22 +8,7 @@ function makePopUp(thiswidth,thisheight,thisDocument,thisWindowName,thisXPositio
 	// Generates a unique window name like "Overview - Hessen" or "Overview - Bund"
 	path_names = window.location.pathname.split('/');
 	generic_window_name = thisWindowName + "-" + path_names[path_names.length -2];
-	if(self.myWindow == null){
-		delete myWindow;
-		myWindow = window.open(thisDocument,generic_window_name,myProperty);
-		if(myWindow != null){
-			myWindow.focus();
-		}
-	}
-	else{
-		if(!(myWindow.closed)){
-			self.myWindow.close();
-			myWindow  = window.open(thisDocument,generic_window_name,myProperty);
-		}
-		else{
-			myWindow  = window.open(thisDocument,generic_window_name,myProperty);
-		}
-	}
+    window.open(thisDocument,generic_window_name,myProperty);
 }
 
 
