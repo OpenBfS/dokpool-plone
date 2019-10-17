4de4cfed6efc78ee074579f80ce60f7b3460a42e
Steffen Lindner <mail@steffen-lindner.de>
2019-10-17 15:29:39 +0200

    Show the journals in the overview portlet

--- Plone/src/elan.esd/elan/esd/portlets/overview.py
+++ Plone/src/elan.esd/elan/esd/portlets/overview.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from docpool.event.utils import getScenariosForCurrentUser
 from elan.esd import DocpoolMessageFactory as _
 from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.interface import implementer
-
+import plone.api as api
 
 # This interface defines the configurable options (if any) for the portlet.
 # It will be used to generate add and edit forms. In this case, we don't
@@ -61,10 +61,23 @@ class Renderer(base.Renderer):
                     'ELANDocCollection',
                     'Dashboard',
                     'SituationOverview',
-                    'Journal',
                 ]
             }
         )
+        # Get the active journals for this Scenario
+        scenarios = getScenariosForCurrentUser(self.context)
+        journals = []
+        # User could select more than one scenario
+        for scenario in scenarios:
+            event_brain = api.content.find(portal_type='DPEvent', id=scenario)
+            if not event_brain:
+                pass
+            event_path = '/'.join(event_brain[0].getObject().getPhysicalPath())
+            for journal in api.content.find(portal_type='Journal', path=event_path):
+                journals.append(journal)
+        if journals:
+            # fc is a LazyMap
+            fc = list(fc) + journals
         return [o for o in fc if o.id not in ('recent', 'overview')]
 
 
