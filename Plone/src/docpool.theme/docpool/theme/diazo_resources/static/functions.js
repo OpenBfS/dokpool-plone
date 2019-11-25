6f8d37a5b2223df639150690f377ec5392a6bb61
Steffen Lindner <mail@steffen-lindner.de>
2019-11-25 15:29:41 +0100

    Refocus open popup - fixes ticket part 2

--- Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
+++ Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
@@ -1,18 +1,25 @@
-var LastWindowOpened = "";
+// On the same site it saves the popup
+var windowObjectReference = null;
 
 var thisY=0;
 var thisX=0;
 
-function makePopUp(thiswidth,thisheight,thisDocument,thisWindowName,thisXPosition,thisYPosition,thisScrollbar,thisResize) {
-	myProperty = 'toolbar=0,location=0,directories=0,status=0,scrollbars=' + thisScrollbar + ',resizable=' + thisResize + ',width=' + thiswidth + ',height=' + thisheight + ',top=' + thisY + ',left=' + thisX;
+function makePopUp(thiswidth, thisheight, thisDocument, thisWindowName, thisXPosition, thisYPosition, thisScrollbar, thisResize) {
+	'use strict';
+	var myProperty = 'toolbar=0,location=0,directories=0,status=0,scrollbars=' + thisScrollbar + ',resizable=' + thisResize + ',width=' + thiswidth + ',height=' + thisheight + ',top=' + thisY + ',left=' + thisX;
 	// Generates a unique window name like "Overview - Hessen" or "Overview - Bund"
-	path_names = window.location.pathname.split('/');
-	generic_window_name = thisWindowName + "-" + path_names[path_names.length -2];
-    window.open(thisDocument,generic_window_name,myProperty);
+	var path_names = window.location.pathname.split('/');
+	var generic_window_name = thisWindowName + "-" + path_names[path_names.length - 2];
+	// Test if the popup is closed, if not open
+    if(windowObjectReference == null || windowObjectReference.closed) {
+        windowObjectReference = window.open(thisDocument, generic_window_name, myProperty);
+    } else {
+        windowObjectReference.focus();
+    }
 }
 
-
 function go_to(url) {
+	'use strict';
 	if (opener != null) {
 		opener.focus(); 	 
 		opener.document.location = url;
