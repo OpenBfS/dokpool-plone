c8f2cc543fe58d825c3fa1ed717d9612e6932859
Steffen Lindner <mail@steffen-lindner.de>
2019-12-03 20:03:57 +0100

    Fix to handle multiple popups

--- Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
+++ Plone/src/docpool.theme/docpool/theme/diazo_resources/static/functions.js
@@ -1,31 +1,28 @@
 var thisY=0;
 var thisX=0;
-
-function getPopup(name) {
-    'use strict';
-    //Get the window
-    let chron_bund=window.open('',name,'');
-    // The closed property is true if the window is closed.
-    if (!chron_bund.closed){
-        return chron_bund
-    }
-}
-
 function makePopUp(thiswidth, thisheight, thisDocument, thisWindowName, thisXPosition, thisYPosition, thisScrollbar, thisResize) {
     'use strict';
-    let myProperty = 'toolbar=0,location=0,directories=0,status=0,scrollbars=' + thisScrollbar + ',resizable=' + thisResize + ',width=' + thiswidth + ',height=' + thisheight + ',top=' + thisY + ',left=' + thisX;
+    var myProperty = 'toolbar=0,location=0,directories=0,status=0,scrollbars=' + thisScrollbar + ',resizable=' + thisResize + ',width=' + thiswidth + ',height=' + thisheight + ',top=' + thisY + ',left=' + thisX;
+    var portal_root = $("body").data('portal-url');
+    // Removes the host - shows only path
+    var path_names = window.location.href.replace(portal_root,"");
     // Generates a unique window name like "Overview - Hessen" or "Overview - Bund"
-    let path_names = window.location.pathname.split('/');
-    let generic_window_name = thisWindowName + "-" + path_names[path_names.length - 2];
-    let open_popup= getPopup(generic_window_name);
-    if (typeof (open_popup) == 'undefined'){
-        // Popup does not exists, we open it
-        console.log('Open Popup: ' + generic_window_name);
-        window.open(thisDocument, generic_window_name, myProperty);
-
-    } else {
-        console.log('Focus Popup:' + generic_window_name);
-        open_popup.focus();
+    var generic_window_name = thisWindowName + "-" + path_names.split('/')[1];
+    window['popup_' + generic_window_name] = null;
+    // No popup exists
+    if (window['open_' + generic_window_name] === false || window['open_' + generic_window_name] === undefined) {
+        window['popup_' + generic_window_name] = window.open(thisDocument, generic_window_name, myProperty);
+        window['open_' + generic_window_name] = true;
+        window['popup_' + generic_window_name].focus();
+    }
+    // Popup was closed and will be reopened
+    if (window['popup_' + generic_window_name] && window['popup_' + generic_window_name].closed === true) {
+        window['popup_' + generic_window_name] = window.open(thisDocument, generic_window_name, myProperty);
+        window['popup_' + generic_window_name].focus();
+    }
+    // Popup is open and will be focused
+    if (window['popup_' + generic_window_name] && window['popup_' + generic_window_name].closed === false) {
+        window['popup_' + generic_window_name].focus();
     }
 }
 
