9836eb4b955172d819acc027a612fc629689823e
Steffen Lindner <mail@steffen-lindner.de>
2020-02-21 16:47:58 +0100

    Add i18n base / rei switch and translations

--- Plone/src/docpool.base/docpool/base/browser/collection.py
+++ Plone/src/docpool.base/docpool/base/browser/collection.py
@@ -1,8 +1,15 @@
-from docpool.base.utils import extendOptions
+# -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+<<<<<<< HEAD
 from plone import api
+=======
+from docpool.base.utils import extendOptions
+from plone import api
+from zope.i18n import translate
+
+>>>>>>> Add i18n base / rei switch and translations
 
 class CollectionDocView(BrowserView):
     """Default view
@@ -32,6 +39,22 @@ class CollectionDocView(BrowserView):
                 request=self.request,
             )
             return view()
+
+    def is_rei_workflow(self, doc):
+        wf_tool = api.portal.get_tool('portal_workflow')
+        workflow = wf_tool.getWorkflowsFor(doc)[0]
+        rei_wfs = ['rei_review_workflow_alternative', 'rei_review_workflow_standard']
+        import pdb; pdb.set_trace()
+        if workflow.id in rei_wfs:
+            return True
+        return False
+
+    def translate_wf_action(self, doc, wf_action):
+        translation_domain = 'docpool.base'
+        if self.is_rei_workflow(doc):
+            translation_domain = 'docpool.rei'
+        wf_state = wf_action['title']
+        return translate(wf_state, domain=translation_domain, context=self.request)
 
 
 class CollectionlistitemView(BrowserView):
