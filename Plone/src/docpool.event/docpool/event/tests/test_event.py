b99867024fc79f402d9423d92a2cd97e1dae0357
Philip Bauer <bauer@starzel.de>
2020-02-24 13:11:57 +0100

    test journal roles and permissions (#3137)

--- Plone/src/docpool.event/docpool/event/tests/test_event.py
+++ Plone/src/docpool.event/docpool/event/tests/test_event.py
@@ -55,3 +55,62 @@ class TestEvent(unittest.TestCase):
         event = container['routinemode']
         with self.assertRaises(RuntimeError):
             api.content.delete(event)
+
+    def test_journals_for_event(self):
+        docpool = self.portal['test_docpool']
+        container = docpool['contentconfig']['scen']
+        event = api.content.create(
+            container=container,
+            type='DPEvent',
+            id='test_event',
+            title=u'Test Event',
+            )
+        # check that 4 journals were added
+        self.assertEqual(
+            event.keys(),
+            ['journal1', 'journal2', 'journal3', 'journal4'])
+        self.assertEqual(
+            [i.title for i in event.contentValues()],
+            [u'Einsatztagebuch BfS', u'Einsatztagebuch RLZ', u'Einsatztagebuch SSK', u'Einsatztagebuch Messdienste'])
+
+        # check the local roles for each journal
+        self.assertEqual(
+            event.journal1.get_local_roles(),
+            (('test_docpool_Journal1_Editors', ('JournalEditor',)), ('test_docpool_Journal1_Readers', ('JournalReader',)), ('test_user_1_', ('Owner',))),
+        )
+        self.assertEqual(
+            event.journal2.get_local_roles(),
+            (('test_docpool_Journal2_Editors', ('JournalEditor',)), ('test_docpool_Journal2_Readers', ('JournalReader',)), ('test_user_1_', ('Owner',))),
+        )
+        self.assertEqual(
+            event.journal3.get_local_roles(),
+            (('test_docpool_Journal3_Editors', ('JournalEditor',)), ('test_docpool_Journal3_Readers', ('JournalReader',)), ('test_user_1_', ('Owner',))),
+        )
+        self.assertEqual(
+            event.journal4.get_local_roles(),
+            (('test_docpool_Journal4_Editors', ('JournalEditor',)), ('test_docpool_Journal4_Readers', ('JournalReader',)), ('test_user_1_', ('Owner',))),
+        )
+
+        # Check that 2 groups each for up to 5 journals actually exist
+        self.assertTrue(api.group.get('test_docpool_Journal1_Readers'))
+        self.assertTrue(api.group.get('test_docpool_Journal2_Readers'))
+        self.assertTrue(api.group.get('test_docpool_Journal3_Readers'))
+        self.assertTrue(api.group.get('test_docpool_Journal4_Readers'))
+        self.assertTrue(api.group.get('test_docpool_Journal5_Readers'))
+        self.assertIsNone(api.group.get('test_docpool_Journal6_Readers'))
+
+        self.assertTrue(api.group.get('test_docpool_Journal1_Editors'))
+        self.assertTrue(api.group.get('test_docpool_Journal2_Editors'))
+        self.assertTrue(api.group.get('test_docpool_Journal3_Editors'))
+        self.assertTrue(api.group.get('test_docpool_Journal4_Editors'))
+        self.assertTrue(api.group.get('test_docpool_Journal5_Editors'))
+        self.assertIsNone(api.group.get('test_docpool_Journal6_Editors'))
+
+        # check that permissions are as defined in dp_journal_workflow
+        self.assertEqual(
+            event.journal1._View_Permission,
+            ('Manager', 'Owner', 'ContentAdmin', 'Site Administrator', 'DocPoolAdmin', 'EventEditor', 'JournalEditor', 'JournalReader'))
+        self.assertEqual(
+            event.journal1._Modify_portal_content_Permission,
+            ('Manager', 'Owner', 'ContentAdmin', 'Site Administrator', 'DocPoolAdmin', 'EventEditor', 'JournalEditor'))
+
