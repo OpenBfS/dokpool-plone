8f772bd7d5fd8b132e07ef18414848bf48ed27c7
Philip Bauer <bauer@starzel.de>
2020-06-10 13:22:15 +0200

    working transfer-form

--- Plone/src/docpool.transfers/docpool/transfers/browser/dpdocument_transfer_form.py
+++ Plone/src/docpool.transfers/docpool/transfers/browser/dpdocument_transfer_form.py
@@ -5,26 +5,27 @@ from Products.CMFPlone.utils import log, log_exc
 from docpool.transfers.config import TRANSFERS_APP
 from Products.Five.browser import BrowserView
 from docpool.transfers import DocpoolMessageFactory as _
+from docpool.transfers.behaviors.transferable import ITransferable
 
 
 class TransferForm(BrowserView):
 
 
-    def __call__(self, dpdocid=None, targets=None):
+    def __call__(self, dpdocids=None, targets=None):
         request = self.request
-        if not request.get('submit'):
+        if not request.get('form.button.submit'):
             return self.index()
 
-        if not dpdocid or not targets:
+        if not dpdocids or not targets:
             api.portal.show_message('Not items or targets selected!', request)
             return self.index()
 
-        message = self.transfer_documents(dpdocid, targetIds)
+        message = self.transfer_documents(dpdocids, targets)
         if message:
             api.portal.show_message(message, request)
             return self.index()
 
-        api.portal.show_message('{} Items transfered!'.format(len(dpdocid)), request)
+        api.portal.show_message('{} Items transfered!'.format(len(dpdocids)), request)
         request.response.redirect(self.context.absolute_url())
 
     def getTransferInfos(self):
@@ -34,14 +35,35 @@ class TransferForm(BrowserView):
             obj = api.content.get(path=path)
             if not obj:
                 continue
-            if obj.transferable() and obj.allowedTargets():
-                infos.append((obj.id, obj, obj.doc_extension(TRANSFERS_APP)))
+            try:
+                adapted = ITransferable(obj)
+            except TypeError:
+                continue
+            if adapted.transferable() and adapted.allowedTargets():
+                infos.append((obj.id, obj, adapted))
         return infos
 
-    def transfer_documents(self, dpdocid, targetIds):
+    def transfer_infos(self):
+        targets = []
+        items = []
+        paths = self.request.get('paths', [])
+        for path in paths:
+            obj = api.content.get(path=path)
+            if not obj:
+                continue
+            try:
+                adapted = ITransferable(obj)
+            except TypeError:
+                continue
+            if adapted.transferable() and adapted.allowedTargets():
+                targets.extend(adapted.allowedTargets())
+                items.append(obj)
+        return {'items': items, 'targets': set(targets)}
 
-        doc = self._getOb(dpdocid)
-        dpdoc = doc.doc_extension(TRANSFERS_APP)
-        dpdoc.manage_transfer(targetIds)
-        log("Transfer %s to ChannelIDs: %s" % (doc.Title(), targetIds))
+    def transfer_documents(self, dpdocids, targetIds):
+        for dpdocid in dpdocids:
+            doc = self.context._getOb(dpdocid)
+            dpdoc = doc.doc_extension(TRANSFERS_APP)
+            dpdoc.manage_transfer(targetIds)
+            log("Transfer %s to ChannelIDs: %s" % (doc.title, targetIds))
         return
