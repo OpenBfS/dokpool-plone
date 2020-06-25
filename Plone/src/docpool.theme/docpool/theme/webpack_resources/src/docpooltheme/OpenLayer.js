70fb0d9130721bfcdb231b06782d8678348aec9d
Steffen Lindner <mail@steffen-lindner.de>
2020-06-25 19:26:57 +0200

    Fix typo

--- Plone/src/docpool.theme/docpool/theme/webpack_resources/src/docpooltheme/OpenLayer.js
+++ Plone/src/docpool.theme/docpool/theme/webpack_resources/src/docpooltheme/OpenLayer.js
@@ -1,6 +1,6 @@
 // TODO expose-loader can be updated to 1.0.0 - not yet done
 // https://github.com/webpack-contrib/expose-loader/blob/master/CHANGELOG.md#100-2020-06-23
-import "expose-loader?OpenLayers!exports-loader?OpenLaysers!OpenLayers";
+import "expose-loader?OpenLayers!exports-loader?OpenLayers!OpenLayers";
 import(/* webpackChunkName: "proj4" */ 'proj4');
 import(/* webpackChunkName: "collectivegeo_init" */ 'collectivegeo_init');
 import(/* webpackChunkName: "collectivegeo" */ 'collectivegeo')
