f50f888592347a19d223546097b6db6b35779a6d
Kathrin Probst <kprobst@bfs.de>
2020-07-07 16:09:08 +0200

    re-ordered fields for doksys document (edit/view), masked DOM

--- Plone/src/docpool.doksys/src/docpool/doksys/behaviors/doksysdoc.py
+++ Plone/src/docpool.doksys/src/docpool/doksys/behaviors/doksysdoc.py
@@ -25,6 +25,25 @@ class IDoksysDoc(IDocumentExtension):
     # dexteritytextindexer.searchable('NetworkOperator')  if a field is
     # supposed to be fulltext searchable
 
+    OperationMode = schema.Choice(
+        title=_(u'label_doksys_operation_mode', default=u'Operation Mode'),
+        description=_(u'description_doksys_operation_mode', default=u''),
+        source="docpool.doksys.OperationMode",
+        default=u'Routine',
+        required=True,
+    )
+    read_permission(OperationMode='docpool.doksys.AccessDoksys')
+    write_permission(OperationMode='docpool.doksys.AccessDoksys')
+
+    Purpose = schema.Choice(
+        title=_(u'label_doksys_purpose', default=u'Purpose'),
+        description=_(u'description_doksys_purpose', default=u''),
+        source="docpool.doksys.Purpose",
+        required=False,
+    )
+    read_permission(Purpose='docpool.doksys.AccessDoksys')
+    write_permission(Purpose='docpool.doksys.AccessDoksys')
+
     widget(NetworkOperator=SelectFieldWidget)
     NetworkOperator = schema.List(
         title=_(u'label_doksys_network_operator', default=u'Network Operator'),
@@ -37,19 +56,6 @@ class IDoksysDoc(IDocumentExtension):
     )
     read_permission(NetworkOperator='docpool.doksys.AccessDoksys')
     write_permission(NetworkOperator='docpool.doksys.AccessDoksys')
-
-    widget(Dom=SelectFieldWidget)
-    Dom = schema.List(
-        title=_(u'label_doksys_Dom', default=u'Description of Measurement'),
-        description=_(u'description_doksys_Dom', default=u''),
-        value_type=schema.Choice(
-            source="docpool.doksys.Dom",
-        ),
-        required=False,
-        missing_value=[],
-    )
-    read_permission(Dom='docpool.doksys.AccessDoksys')
-    write_permission(Dom='docpool.doksys.AccessDoksys')
 
     widget(LegalBase=CheckBoxFieldWidget)
     LegalBase = schema.List(
@@ -75,6 +81,87 @@ class IDoksysDoc(IDocumentExtension):
     read_permission(MeasuringProgram='docpool.doksys.AccessDoksys')
     write_permission(MeasuringProgram='docpool.doksys.AccessDoksys')
 
+    widget(DataType=CheckBoxFieldWidget)
+    DataType = schema.List(
+        title=_(u'label_doksys_data_type', default=u'Data Type'),
+        description=_(u'description_doksys_data_type', default=u''),
+        value_type=schema.Choice(
+            source="docpool.doksys.DataType",
+        ),
+        required=False,
+        missing_value=[],
+    )
+    read_permission(DataType='docpool.doksys.AccessDoksys')
+    write_permission(DataType='docpool.doksys.AccessDoksys')
+
+    widget(SampleType=SelectFieldWidget)
+    SampleType = schema.List(
+        title=_(u'label_doksys_sample_type', default=u'Sample Type'),
+        description=_(u'description_doksys_sample_type', default=u''),
+        value_type=schema.Choice(
+            source="docpool.doksys.SampleType",
+        ),
+        required=False,
+        missing_value=[],
+    )
+    read_permission(SampleType='docpool.doksys.AccessDoksys')
+    write_permission(SampleType='docpool.doksys.AccessDoksys')
+
+    widget(Dom=SelectFieldWidget)
+    Dom = schema.List(
+        title=_(u'label_doksys_Dom', default=u'Description of Measurement'),
+        description=_(u'description_doksys_Dom', default=u''),
+        value_type=schema.Choice(
+            source="docpool.doksys.Dom",
+        ),
+        required=False,
+        missing_value=[],
+    )
+    read_permission(Dom='docpool.doksys.AccessDoksysDOM')
+    write_permission(Dom='docpool.doksys.AccessDoksysDOM')
+
+    InfoType = schema.Choice(
+        title=_(u'label_doksys_infotype', default=u'InfoType'),
+        description=_(u'description_doksys_infotype', default=u''),
+        source="docpool.doksys.InfoType",
+        required=False,
+    )
+    read_permission(InfoType='docpool.doksys.AccessDoksys')
+    write_permission(InfoType='docpool.doksys.AccessDoksys')
+
+    widget(MeasurementCategory=SelectFieldWidget)
+    MeasurementCategory = schema.List(
+        title=_(
+            u'label_doksys_measurement_category',
+            default=u'Measurement Category'),
+        description=_(u'description_doksys_measurement_category', default=u''),
+        value_type=schema.Choice(
+            source="docpool.doksys.MeasurementCategory",
+        ),
+        required=False,
+        missing_value=[],
+    )
+    read_permission(MeasurementCategory='docpool.doksys.AccessDoksys')
+    write_permission(MeasurementCategory='docpool.doksys.AccessDoksys')
+
+    Duration = schema.Choice(
+        title=_(u'label_doksys_duration', default=u'Duration'),
+        description=_(u'description_doksys_duration', default=u''),
+        source="docpool.doksys.Duration",
+        required=False,
+    )
+    read_permission(Duration='docpool.doksys.AccessDoksys')
+    write_permission(Duration='docpool.doksys.AccessDoksys')
+
+    Status = schema.Choice(
+        title=_(u'label_doksys_status', default=u'Status'),
+        description=_(u'description_doksys_status', default=u''),
+        source="docpool.doksys.Status",
+        required=False,
+    )
+    read_permission(Status='docpool.doksys.AccessDoksys')
+    write_permission(Status='docpool.doksys.AccessDoksys')
+
     SamplingBegin = schema.Datetime(
         title=_(u'label_doksys_sampling_begin', default=u'Sampling Begin'),
         description=_(u'description_doksys_sampling_begin', default=u''),
@@ -90,15 +177,6 @@ class IDoksysDoc(IDocumentExtension):
     )
     read_permission(SamplingEnd='docpool.doksys.AccessDoksys')
     write_permission(SamplingEnd='docpool.doksys.AccessDoksys')
-
-    Purpose = schema.Choice(
-        title=_(u'label_doksys_purpose', default=u'Purpose'),
-        description=_(u'description_doksys_purpose', default=u''),
-        source="docpool.doksys.Purpose",
-        required=False,
-    )
-    read_permission(Purpose='docpool.doksys.AccessDoksys')
-    write_permission(Purpose='docpool.doksys.AccessDoksys')
 
     TrajectoryStartLocation = schema.TextLine(
         title=_(
@@ -147,84 +225,6 @@ class IDoksysDoc(IDocumentExtension):
     read_permission(TrajectoryEndTime='docpool.doksys.AccessDoksys')
     write_permission(TrajectoryEndTime='docpool.doksys.AccessDoksys')
 
-    Status = schema.Choice(
-        title=_(u'label_doksys_status', default=u'Status'),
-        description=_(u'description_doksys_status', default=u''),
-        source="docpool.doksys.Status",
-        required=False,
-    )
-    read_permission(Status='docpool.doksys.AccessDoksys')
-    write_permission(Status='docpool.doksys.AccessDoksys')
-
-    OperationMode = schema.Choice(
-        title=_(u'label_doksys_operation_mode', default=u'Operation Mode'),
-        description=_(u'description_doksys_operation_mode', default=u''),
-        source="docpool.doksys.OperationMode",
-        default=u'Routine',
-        required=True,
-    )
-    read_permission(OperationMode='docpool.doksys.AccessDoksys')
-    write_permission(OperationMode='docpool.doksys.AccessDoksys')
-
-    widget(DataType=CheckBoxFieldWidget)
-    DataType = schema.List(
-        title=_(u'label_doksys_data_type', default=u'Data Type'),
-        description=_(u'description_doksys_data_type', default=u''),
-        value_type=schema.Choice(
-            source="docpool.doksys.DataType",
-        ),
-        required=False,
-        missing_value=[],
-    )
-    read_permission(DataType='docpool.doksys.AccessDoksys')
-    write_permission(DataType='docpool.doksys.AccessDoksys')
-
-    widget(SampleType=SelectFieldWidget)
-    SampleType = schema.List(
-        title=_(u'label_doksys_sample_type', default=u'Sample Type'),
-        description=_(u'description_doksys_sample_type', default=u''),
-        value_type=schema.Choice(
-            source="docpool.doksys.SampleType",
-        ),
-        required=False,
-        missing_value=[],
-    )
-    read_permission(SampleType='docpool.doksys.AccessDoksys')
-    write_permission(SampleType='docpool.doksys.AccessDoksys')
-
-    widget(MeasurementCategory=SelectFieldWidget)
-    MeasurementCategory = schema.List(
-        title=_(
-            u'label_doksys_measurement_category',
-            default=u'Measurement Category'),
-        description=_(u'description_doksys_measurement_category', default=u''),
-        value_type=schema.Choice(
-            source="docpool.doksys.MeasurementCategory",
-        ),
-        required=False,
-        missing_value=[],
-    )
-    read_permission(MeasurementCategory='docpool.doksys.AccessDoksys')
-    write_permission(MeasurementCategory='docpool.doksys.AccessDoksys')
-
-    Duration = schema.Choice(
-        title=_(u'label_doksys_duration', default=u'Duration'),
-        description=_(u'description_doksys_duration', default=u''),
-        source="docpool.doksys.Duration",
-        required=False,
-    )
-    read_permission(Duration='docpool.doksys.AccessDoksys')
-    write_permission(Duration='docpool.doksys.AccessDoksys')
-
-    InfoType = schema.Choice(
-        title=_(u'label_doksys_infotype', default=u'InfoType'),
-        description=_(u'description_doksys_infotype', default=u''),
-        source="docpool.doksys.InfoType",
-        required=False,
-    )
-    read_permission(InfoType='docpool.doksys.AccessDoksys')
-    write_permission(InfoType='docpool.doksys.AccessDoksys')
-
     Area = schema.Choice(
         title=_(u'label_doksys_area', default=u'Area'),
         description=_(u'description_doksys_area', default=u''),
@@ -246,6 +246,28 @@ class DoksysDoc(FlexibleView):
         self.context = context
         self.request = context.REQUEST
 
+    def _get_Purpose(self):
+        return self.context.Purpose
+
+    def _set_Purpose(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.Purpose = value
+
+    Purpose = property(_get_Purpose, _set_Purpose)
+
+    def _get_OperationMode(self):
+        return self.context.OperationMode
+
+    def _set_OperationMode(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.OperationMode = value
+
+    OperationMode = property(_get_OperationMode, _set_OperationMode)
+
     def _get_NetworkOperator(self):
         return self.context.NetworkOperator
 
@@ -256,6 +278,54 @@ class DoksysDoc(FlexibleView):
         context.NetworkOperator = value
 
     NetworkOperator = property(_get_NetworkOperator, _set_NetworkOperator)
+
+    def _get_LegalBase(self):
+        return self.context.LegalBase
+
+    def _set_LegalBase(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.LegalBase = value
+
+    LegalBase = property(_get_LegalBase, _set_LegalBase)
+
+    def _get_MeasuringProgram(self):
+        return self.context.MeasuringProgram
+
+    def _set_MeasuringProgram(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.MeasuringProgram = value
+
+    MeasuringProgram = property(_get_MeasuringProgram, _set_MeasuringProgram)
+
+    def _get_DataType(self):
+        return self.context.DataType
+
+    def _set_DataType(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.DataType = value
+
+    DataType = property(_get_DataType, _set_DataType)
+
+    def _get_SampleType(self):
+        return self.context.SampleType
+
+    def _set_SampleType(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.SampleType = value
+
+    SampleType = property(_get_SampleType, _set_SampleType)
+
+    def sample_type_display(self):
+        voc = getUtility(IVocabularyFactory, 'docpool.doksys.SampleType')()
+        return u', '.join(voc.getTerm(i).title for i in self.SampleType)
 
     def _get_Dom(self):
         return self.context.Dom
@@ -268,16 +338,49 @@ class DoksysDoc(FlexibleView):
 
     Dom = property(_get_Dom, _set_Dom)
 
-    def _get_LegalBase(self):
-        return self.context.LegalBase
+    def _get_InfoType(self):
+        return self.context.InfoType
 
-    def _set_LegalBase(self, value):
+    def _set_InfoType(self, value):
         if not value:
             return
         context = aq_inner(self.context)
-        context.LegalBase = value
+        context.InfoType = value
 
-    LegalBase = property(_get_LegalBase, _set_LegalBase)
+    InfoType = property(_get_InfoType, _set_InfoType)
+
+    def _get_MeasurementCategory(self):
+        return self.context.MeasurementCategory
+
+    def _set_MeasurementCategory(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.MeasurementCategory = value
+
+    MeasurementCategory = property(_get_MeasurementCategory,_set_MeasurementCategory)
+
+    def _get_Duration(self):
+        return self.context.Duration
+
+    def _set_Duration(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.Duration = value
+
+    Duration = property(_get_Duration, _set_Duration)
+
+    def _get_Status(self):
+        return self.context.Status
+
+    def _set_Status(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.Status = value
+
+    Status = property(_get_Status, _set_Status)
 
     def _get_SamplingBegin(self):
         return self.context.SamplingBegin
@@ -300,28 +403,6 @@ class DoksysDoc(FlexibleView):
         context.SamplingEnd = value
 
     SamplingEnd = property(_get_SamplingEnd, _set_SamplingEnd)
-
-    def _get_MeasuringProgram(self):
-        return self.context.MeasuringProgram
-
-    def _set_MeasuringProgram(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.MeasuringProgram = value
-
-    MeasuringProgram = property(_get_MeasuringProgram, _set_MeasuringProgram)
-
-    def _get_Purpose(self):
-        return self.context.Purpose
-
-    def _set_Purpose(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.Purpose = value
-
-    Purpose = property(_get_Purpose, _set_Purpose)
 
     def _get_TrajectoryStartLocation(self):
         return self.context.TrajectoryStartLocation
@@ -374,100 +455,6 @@ class DoksysDoc(FlexibleView):
     TrajectoryEndTime = property(
         _get_TrajectoryEndTime,
         _set_TrajectoryEndTime)
-
-    def _get_Status(self):
-        return self.context.Status
-
-    def _set_Status(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.Status = value
-
-    Status = property(_get_Status, _set_Status)
-
-    def _get_OperationMode(self):
-        return self.context.OperationMode
-
-    def _set_OperationMode(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.OperationMode = value
-
-    OperationMode = property(_get_OperationMode, _set_OperationMode)
-
-    def _get_DataType(self):
-        return self.context.DataType
-
-    def _set_DataType(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.DataType = value
-
-    DataType = property(_get_DataType, _set_DataType)
-
-    def _get_SampleType(self):
-        return self.context.SampleType
-
-    def _set_SampleType(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.SampleType = value
-
-    SampleType = property(_get_SampleType, _set_SampleType)
-
-    def sample_type_display(self):
-        voc = getUtility(IVocabularyFactory, 'docpool.doksys.SampleType')()
-        return u', '.join(voc.getTerm(i).title for i in self.SampleType)
-
-    def _get_MeasurementCategory(self):
-        return self.context.MeasurementCategory
-
-    def _set_MeasurementCategory(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.MeasurementCategory = value
-
-    MeasurementCategory = property(
-        _get_MeasurementCategory,
-        _set_MeasurementCategory)
-
-    def _get_Duration(self):
-        return self.context.Duration
-
-    def _set_Duration(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.Duration = value
-
-    Duration = property(_get_Duration, _set_Duration)
-
-    def _get_InfoType(self):
-        return self.context.InfoType
-
-    def _set_InfoType(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.InfoType = value
-
-    InfoType = property(_get_InfoType, _set_InfoType)
-
-    def _get_Type(self):
-        return self.context.Type
-
-    def _set_Type(self, value):
-        if not value:
-            return
-        context = aq_inner(self.context)
-        context.Type = value
-
-    Type = property(_get_Type, _set_Type)
 
     def _get_Area(self):
         return self.context.Area
