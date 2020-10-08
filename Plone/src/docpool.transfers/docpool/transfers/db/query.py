6ea9e4fcc2eeb52e9787c21bc4d04522d6cdbca2
Thomas Lotze <thomas@thomas-lotze.net>
2020-10-08 09:12:10 +0200

    re #2601: clean up code of moved sql query method

--- Plone/src/docpool.transfers/docpool/transfers/db/query.py
+++ Plone/src/docpool.transfers/docpool/transfers/db/query.py
@@ -14,12 +14,8 @@ def allowed_targets(context):
     and my current version must not have been transferred.
     """
     esd_uid = context.myDocumentPool().UID()
-    # print esd_uid
     dto = context.docTypeObj()
-    dt_id = dto and dto.id or '---'
-    # print dt_id
-    m = context.getMdate()
-    # print m
+    dt_id = dto.id if dto else '---'
     q = (
         __session__.query(Channel)
         .outerjoin(Channel.permissions)
@@ -38,14 +34,12 @@ def allowed_targets(context):
                 ~Channel.sends.any(
                     and_(
                         SenderLog.document_uid == context.UID(),
-                        SenderLog.timestamp > m,
+                        SenderLog.timestamp > context.getMdate(),
                     )
                 ),
             )
         )
         .order_by('esd_from_title')
     )
-    # print q.statement
     targets = q.all()
-    # print len(targets)
     return targets
