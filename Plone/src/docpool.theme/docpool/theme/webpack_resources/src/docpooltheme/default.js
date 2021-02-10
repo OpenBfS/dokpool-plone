d811bee2c550fb439c90dd3981cbef2ab5d686b3
Thomas Lotze <thomas@thomas-lotze.net>
2021-02-10 16:23:38 +0100

    simplify bookkeeping of open popup windows

--- Plone/src/docpool.theme/docpool/theme/webpack_resources/src/docpooltheme/default.js
+++ Plone/src/docpool.theme/docpool/theme/webpack_resources/src/docpooltheme/default.js
@@ -15,7 +15,6 @@ function makePopUp(thiswidth, thisheight, thisDocument, thisWindowName, thisXPos
     var path_names = window.location.href.replace(portal_root,"");
     // Generates a unique window name like "Overview - Hessen" or "Overview - Bund"
     var generic_window_name = thisWindowName + "-" + path_names.split('/')[1];
-    window['popup_' + generic_window_name] = null;
     // Add to open_popups
     // XXX open_popups should really be a set of unique names
     let open_popups = JSON.parse(localStorage.getItem("open_popups"));
@@ -24,26 +23,11 @@ function makePopUp(thiswidth, thisheight, thisDocument, thisWindowName, thisXPos
     }
     open_popups.push(generic_window_name);
     localStorage.setItem("open_popups", JSON.stringify(open_popups));
-    // No popup exists
-    if (window['open_' + generic_window_name] === false || window['open_' + generic_window_name] === undefined) {
+    // Open popup if it doesn't yet exist, or reopen if it was closed
+    if (!window['popup_' + generic_window_name] || window['popup_' + generic_window_name].closed === true) {
         window['popup_' + generic_window_name] = window.open(thisDocument, generic_window_name, myProperty);
-        window['open_' + generic_window_name] = true;
-        window['popup_' + generic_window_name].focus();
     }
-    // Popup was closed and will be reopened
-    if (window['popup_' + generic_window_name] && window['popup_' + generic_window_name].closed === true) {
-        window['popup_' + generic_window_name] = window.open(thisDocument, generic_window_name, myProperty);
-        window['popup_' + generic_window_name].focus();
-    }
-    // Popup is open and will be focused
-    if (window['popup_' + generic_window_name] && window['popup_' + generic_window_name].closed === false) {
-        window['popup_' + generic_window_name].focus();
-    }
-    // Popup open and no reload happening
-    if (window['open_' + generic_window_name]){
-        window['popup_' + generic_window_name] = window.open(thisDocument, generic_window_name, myProperty);
-        window['popup_' + generic_window_name].focus();
-    }
+    window['popup_' + generic_window_name].focus();
 }
 
 // Todo Needs a place to stay (own file)
