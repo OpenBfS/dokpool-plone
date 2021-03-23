d4ebb3276561d613978c2e228be8b797df0a00c1
Kathrin Probst <kprobst@bfs.de>
2021-03-23 17:33:10 +0100

    edited typing error in setup.py

--- Plone/src/docpool.base/setup.py
+++ Plone/src/docpool.base/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.8.0.d8.0.d8.0.d8.0.d8.0.d8.0.d8.0.d8.0.d',
+version = '1.8.0.dev0',
     version=version,
     description="",
     long_description="""\
