a2e30b5866bad5acd21eb6f931eeccec4b8b4f9b
Thomas Lotze <lotze@starzel.de>
2021-06-17 17:00:50 +0200

    re #4324: introduce global scenario selection as a portal annotation
    * use this information to compute selected scenarios for users
    * reimplement loops over users for adding, closing or removing scenarios
      in terms of a single access to this annotation

--- Plone/src/docpool.event/docpool/event/utils.py
+++ Plone/src/docpool.event/docpool/event/utils.py
@@ -1,6 +1,12 @@
 from docpool.base.utils import getDocumentPoolSite
+from persistent.list import PersistentList
+from persistent.mapping import PersistentMapping
 from plone import api
 from Products.CMFCore.utils import getToolByName
+from zope.annotation.interfaces import IAnnotations
+
+
+ANN_KEY_SCENARIO_SELECTION = 'SCENARIO_SELECTION'
 
 
 def getActiveScenarios(self):
@@ -39,8 +45,16 @@ def getScenariosForCurrentUser(self):
 
 
 def get_scenarios_for_user(self, user):
-    scenarios = user.getProperty("scenarios", [])
-    return scenarios
+    scenarios = set(user.getProperty("scenarios", []))
+    global_scenarios = get_global_scenario_selection()
+    for scen, state in global_scenarios.items():
+        if state == 'selected':
+            scenarios.add(scen)
+        elif state in ('closed', 'removed'):
+            scenarios.discard(scen)
+        else:
+            assert False, '{0} is not a valid scenario selection state'.format(state)
+    return list(scenarios)
 
 
 def setScenariosForCurrentUser(self, scenarios):
@@ -48,3 +62,9 @@ def setScenariosForCurrentUser(self, scenarios):
     """
     user = api.user.get_current()
     user.setMemberProperties({"scenarios": scenarios})
+
+
+def get_global_scenario_selection():
+    portal = api.portal.get()
+    annotations = IAnnotations(portal)
+    return annotations.setdefault(ANN_KEY_SCENARIO_SELECTION, PersistentMapping())
