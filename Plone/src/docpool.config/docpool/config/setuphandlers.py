c016c1d259ac4f6c851d3d82e4331f751b6fbd65
Philip Bauer <bauer@starzel.de>
2022-06-10 22:13:41 +0200

    modernize profile for docpool.config

--- Plone/src/docpool.config/docpool/config/setuphandlers.py
+++ Plone/src/docpool.config/docpool/config/setuphandlers.py
@@ -3,13 +3,7 @@
 
 def post_install(context):
     """Post install script"""
-    if context.readDataFile('docpoolconfig_default.txt') is None:
-        return
-    # Do something during the installation of this package
 
 
 def uninstall(context):
     """Uninstall script"""
-    if context.readDataFile('docpoolconfig_uninstall.txt') is None:
-        return
-    # Do something during the uninstallation of this package
