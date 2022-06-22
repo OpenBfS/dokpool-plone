233dd3e09d872ed9a087b2bf72a6757517774f4d
Thomas Lotze <lotze@starzel.de>
2022-06-22 19:38:30 +0200

    remove unused util function

--- Plone/src/docpool.transfers/docpool/transfers/utils.py
+++ Plone/src/docpool.transfers/docpool/transfers/utils.py
@@ -1,11 +1,4 @@
-from docpool.transfers.config import TRANSFERS_APP
 from plone import api
-
-
-def getTupleForTransfer(self, id):
-    """ """
-    doc = self._getOb(id)
-    return doc, doc.doc_extension(TRANSFERS_APP)
 
 
 def is_sender(obj):
