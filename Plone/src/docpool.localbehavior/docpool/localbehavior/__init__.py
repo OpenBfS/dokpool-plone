b082a266746e4b9c6e8606eeb068fff2dd5b8337
Philip Bauer <bauer@starzel.de>
2022-09-02 14:42:56 +0200

    remove package docpool.localbehavior

--- Plone/src/docpool.localbehavior/docpool/localbehavior/__init__.py
+++
@@ -1,7 +0,0 @@
-from zope.i18nmessageid import MessageFactory as MF
-
-
-# Set up the i18n message factory for our package
-MessageFactory = MF("docpool.localbehavior")
-
-from docpool.localbehavior import patches  # noqa: F401
