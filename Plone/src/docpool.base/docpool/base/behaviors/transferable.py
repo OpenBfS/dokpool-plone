ce20011c93979f1f05b767ea5f7bcf69a5aba3a1
Philip Bauer <bauer@starzel.de>
2022-09-15 09:44:46 +0200

    workon moving transfers to base

---
+++ Plone/src/docpool.base/docpool/base/behaviors/transferable.py
@@ -0,0 +1,434 @@
+from AccessControl import ClassSecurityInfo
+from Acquisition import aq_inner
+from contextlib import contextmanager
+from datetime import datetime
+from DateTime import DateTime
+from docpool.base import DocpoolMessageFactory as _
+from docpool.base.behaviors.transferstype import ITransfersType
+from docpool.base.behaviors.utils import allowed_targets
+from docpool.base.browser.flexible_view import FlexibleView
+from docpool.base.content.archiving import IArchiving
+from docpool.base.content.dpdocument import IDPDocument
+from docpool.base.localbehavior.localbehavior import ILocalBehaviorSupport
+from docpool.base.marker import IImportingMarker
+from docpool.base.utils import _copyPaste
+from docpool.base.utils import execute_under_special_role
+from docpool.base.utils import portalMessage
+from docpool.elan.behaviors.elandocument import IELANDocument
+from docpool.elan.config import ELAN_APP
+from docpool.elan.content.transfers import ensureScenariosInTarget
+from docpool.elan.content.transfers import knowsScen
+from docpool.transfers.config import TRANSFERS_APP
+from logging import getLogger
+from plone import api
+from plone.autoform import directives
+from plone.autoform.directives import read_permission
+from plone.autoform.directives import write_permission
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from Products.CMFCore.interfaces import IActionSucceededEvent
+from zope import schema
+from zope.annotation.interfaces import IAnnotations
+from zope.component import adapter
+from zope.globalrequest import getRequest
+from zope.interface import provider
+
+
+logger = getLogger(__name__)
+
+ANNOTATIONS_KEY = __name__
+
+
+@contextmanager
+def transferring():
+    """Keeps record of a tranfer going on while the code block executes.
+
+    The resource returned is a boolean telling whether a transfer was already going on
+    when entering the code block.
+
+    """
+    annotations = IAnnotations(getRequest()).setdefault(ANNOTATIONS_KEY, {})
+    KEY = "transferring"
+    if annotations.get(KEY, False):
+        yield True
+    else:
+        annotations[KEY] = True
+        try:
+            yield False
+        finally:
+            del annotations[KEY]
+
+
+@provider(IFormFieldProvider)
+class ITransferable(model.Schema):
+    transferred_by = schema.TextLine(
+        title=_("label_dpdocument_transferred_by", default="Transferred by"),
+        description=_("description_dpdocument_transferred_by", default=""),
+        required=False,
+    )
+    directives.omitted("transferred_by")
+    read_permission(transferred_by="docpool.transfers.AccessTransfers")
+    write_permission(transferred_by="docpool.transfers.AccessTransfers")
+
+    transferred = schema.Datetime(
+        title=_("label_dpdocument_transferred", default="Date of last transfer"),
+        description=_("description_dpdocument_transferred", default=""),
+        required=False,
+    )
+    directives.omitted("transferred")
+    read_permission(transferred="docpool.transfers.AccessTransfers")
+    write_permission(transferred="docpool.transfers.AccessTransfers")
+
+    transferLog = schema.Text(
+        title=_("label_dpdocument_transferlog", default="Transfer log"),
+        description=_(
+            "description_dpdocument_transferlog",
+            default="Only used for archived documents.",
+        ),
+        required=False,
+    )
+    directives.omitted("transferLog")
+    read_permission(transferLog="docpool.transfers.AccessTransfers")
+    write_permission(transferLog="docpool.transfers.AccessTransfers")
+
+
+class Transferable(FlexibleView):
+    __allow_access_to_unprotected_subobjects__ = 1
+
+    security = ClassSecurityInfo()
+
+    appname = "transfers"
+
+    def __init__(self, context):
+        self.context = context
+        self.request = context.REQUEST
+
+    def _get_transferred_by(self):
+        return self.context.transferred_by
+
+    def _set_transferred_by(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.transferred_by = value
+
+    transferred_by = property(_get_transferred_by, _set_transferred_by)
+
+    def _get_transferred(self):
+        return getattr(self.context, "transferred", None)
+
+    def _set_transferred(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.transferred = value
+
+    transferred = property(_get_transferred, _set_transferred)
+
+    @property
+    def transferLog(self):
+        return self.context.transferLog
+
+    @transferLog.setter
+    def transferLog(self, value):
+        if not value:
+            return
+        context = aq_inner(self.context)
+        context.transferLog = value
+
+    @property
+    def sender_log(self):
+        return getattr(self.context, "transfer_sender_log", ())
+
+    @sender_log.setter
+    def sender_log(self, value):
+        context = aq_inner(self.context)
+        context.transfer_sender_log = value
+
+    @property
+    def receiver_log(self):
+        return getattr(self.context, "transfer_receiver_log", ())
+
+    @receiver_log.setter
+    def receiver_log(self, value):
+        context = aq_inner(self.context)
+        context.transfer_receiver_log = value
+
+    def isClean(self):
+        """
+        Is this document free for further action like publishing or transfer.
+        @return:
+        """
+        return True
+
+    def changed(self):
+        """ """
+        return self.context.transferred or self.context.getMdate()
+
+    def checkTransferLog(self):
+        """ """
+        return self.context.transferLog
+
+    def transferEvents(self):
+        """Query metadata of past transfers ("transfer events") of the context object."""
+        if IArchiving(self.context).is_archive:
+            logRaw = self.transferLog
+            logRaw = logRaw and logRaw.replace("datetime.datetime", "datetime") or ""
+            return eval(logRaw)
+
+        else:
+            if self.transferred:
+                type_ = "receive"
+                events = reversed(self.receiver_log)
+            else:
+                type_ = "send"
+                events = reversed(self.sender_log)
+                return [
+                    {
+                        "type": type_,
+                        "by": event["user"],
+                        "esd": event["esd_title"],
+                        "timeraw": event["timestamp"],
+                        "time": self.context.toLocalizedTime(
+                            DateTime(event["timestamp"]), long_format=1
+                        ),
+                    }
+                    for event in events
+                ]
+
+    def transferable(self):
+        """
+        If not created by a transfer
+        If published
+        If all subobjects published (TODO: maybe they should even be transferable themselves)
+        If DocType allows it
+        If Object allows it directly
+        """
+        if not is_sender(self.context):
+            return False
+        if self.transferred or IArchiving(self.context).is_archive:
+            return False
+        if api.content.get_state(obj=self.context, default=None) != "published":
+            return False
+        if not self.context.allSubobjectsPublished():
+            return False
+        dto = self.context.docTypeObj()
+        if dto and ITransfersType(dto).allowTransfer:
+            return True
+        return False
+
+    def allowedTargets(self):
+        return allowed_targets(self.context)
+
+    security.declareProtected("Docpool: Send Content", "transferToAll")
+
+    def transferToAll(self):
+        """ """
+        dto = self.context.docTypeObj()
+        dto_transfers = dto.type_extension(TRANSFERS_APP)
+        # Intersect allowed and automatic transfer targets while keeping the order of
+        # allowed targets and computing the automatic ones only once.
+        automatic = set(dto_transfers.automaticTransferTargets)
+        targets = [t for t in self.allowedTargets() if t in automatic]
+
+        source_path = "/".join(self.context.getPhysicalPath())
+        if targets:
+            logger.info(f"Transfer {source_path} to up to {len(targets)} targets.")
+            self.transferToTargets(targets)
+        else:
+            logger.info(f"No transfer targets found for {source_path}.")
+
+    security.declareProtected("Docpool: Send Content", "transferToTargets")
+
+    def transferToTargets(self, targets=[]):
+        """
+        1) Determine all transfer folder objects.
+        2) Put a copy of me in each of them, preserving timestamps.
+        3) Add transfer information the copies.
+        4) Add entries to sender logs.
+        5) Make sure document types and scenarios exist in the target ESDs.
+        6) Set workflow state of the copies according to folder permissions.
+        7) Add entries to receiver logs.
+        """
+
+        def error_message(esd_to_title, msg):
+            prefix = _("No transfer to")
+            portalMessage(self.context, f"{prefix} {esd_to_title}. {msg}", type="error")
+
+        def doIt():
+            timestamp = datetime.now()
+            userinfo_string = self.context._getUserInfoString(plain=True)
+            dto = self.context.docTypeObj()
+            elanobj = IELANDocument(self.context)
+
+            for target in targets:
+                # 1) Determine target transfer folder object.
+                transfer_folder = api.content.get(UID=target)
+                esd_to_title = transfer_folder.myDocumentPool().Title()
+
+                # Check permissions:
+                # a) Is my DocType accepted, are unknown DocTypes accepted?
+                udt_ok = transfer_folder.unknownDtDefault != "block"
+                if not udt_ok:
+                    # check my precise DocType
+                    if not transfer_folder.acceptsDT(dto.getId()):
+                        error_message(esd_to_title, _("Doc type not accepted."))
+                        continue
+
+                # b) Is my Scenario known, are unknown Scenarios accepted?
+                scen_ok = transfer_folder.unknownScenDefault != "block"
+                if not scen_ok:
+                    # check my precise Scenario
+                    # FIXME: ELAN dependency
+                    # TODO The following is inefficient in that it creates a list of
+                    # full objects, but it effectively filters elanobj.scenarios for
+                    # those that actually exist in the catalog. Is this necessary?
+                    scens = elanobj.myScenarioObjects()
+                    if scens:
+                        scen_id = scens[0].getId()
+                        if not knowsScen(transfer_folder, scen_id):
+                            error_message(
+                                esd_to_title,
+                                _("Unknown scenario not accepted."),
+                            )
+                            continue
+                    else:
+                        error_message(esd_to_title, _("Document has no scenario."))
+                        continue
+
+                # At this point, transfer is allowed.
+                logger.info(
+                    "Transfer {} to {}.".format(
+                        "/".join(self.context.getPhysicalPath()),
+                        esd_to_title,
+                    )
+                )
+
+                # 2) Put a copy of me in transfer folder, preserving timestamps.
+                new_id = _copyPaste(self.context, transfer_folder)
+                my_copy = transfer_folder._getOb(new_id)
+                behaviors = set(ILocalBehaviorSupport(self.context).local_behaviors)
+                behaviors.add(ELAN_APP)
+                ILocalBehaviorSupport(my_copy).local_behaviors = list(behaviors)
+
+                # 3) Add transfer information to the copies.
+                my_copy.transferred = timestamp
+                my_copy.transferred_by = userinfo_string
+
+                # 4) Add entry to sender log.
+                scenario_ids = ", ".join(elanobj.scenarios or ())
+                self.sender_log += (
+                    dict(
+                        timestamp=timestamp,
+                        user=userinfo_string,
+                        scenario_ids=scenario_ids,
+                        esd_title=esd_to_title,
+                        transferfolder_uid=transfer_folder.UID(),
+                    ),
+                )
+
+                # 5) Make sure document type and scenarios exist in the target ESD and
+                #    are in a suitable state.
+                transfer_folder.ensureDocTypeInTarget(dto)
+
+                ensureScenariosInTarget(self.context, my_copy)
+
+                # 6) Set workflow state of the copy according to folder permissions.
+                ITransferable(my_copy).ensureState()
+                my_copy.reindexObject()
+
+                # 7) Add entry to receiver log.
+                elancopy = IELANDocument(my_copy)
+                scenario_ids = ", ".join(elancopy.scenarios or ())
+                self.receiver_log += (
+                    dict(
+                        timestamp=timestamp,
+                        user=userinfo_string,
+                        scenario_ids=scenario_ids,
+                        esd_title=transfer_folder.getSendingESD().Title(),
+                    ),
+                )
+
+                msg = _(
+                    "Transferred to ${target_title}",
+                    mapping={"target_title": esd_to_title},
+                )
+                api.portal.show_message(msg, self.request)
+
+        execute_under_special_role(self.context, "Manager", doIt)
+
+    def ensureState(self):
+        """Put a transferred object in a state corresponding to the doctype permission.
+
+        If there is no restriction on the transfer folder (permission is 'publish'),
+        make sure workflow state of the copy is private if permission is 'needs
+        confirmation' but public if it is 'publish'.
+
+        """
+        if self.transferred:
+            tf = self.context.myDPTransferFolder()
+            dtObj = self.context.docTypeObj()
+            tstate = api.content.get_state(obj=dtObj)
+            if tstate == "published":  # we do this for valid types only
+                # determine the applicable permission
+                perm = tf.doctypePermissions.get(dtObj.getId())
+                dstate = api.content.get_state(self.context)
+                if dstate == "private" and perm == "publish":
+                    api.content.transition(self.context, "publish")
+                if dstate == "published" and perm == "confirm":
+                    api.content.transition(self.context, "retract")
+            uscn = IELANDocument(self.context).unknownScenario()
+            if uscn:
+                # Documents with unknown scenarios must be private
+                try:
+                    api.content.transition(self.context, "retract")
+                except BaseException:
+                    pass
+
+
+@adapter(IDPDocument, IActionSucceededEvent)
+def automatic_transfer_on_publish(obj, event=None):
+    """ """
+    if event and event.action == "publish":
+        automatic_transfer(obj)
+
+
+def automatic_transfer(obj):
+    if IArchiving(obj).is_archive:
+        # In the process of archiving an event, its associated documents are copied and
+        # afterwards applied a workflow transition in order to restore their original
+        # workflow state. Objects published for this reason should not be transferred.
+        return
+    if IImportingMarker.providedBy(getRequest()):
+        return
+
+    try:
+        tObj = ITransferable(obj)  # Try behaviour
+    except BaseException:
+        return
+
+    with transferring() as already_transferring:
+        if already_transferring:
+            return
+
+        logger.info(
+            'Automatic transfer of "{}" from {}'.format(
+                obj.Title(),
+                "/".join(obj.getPhysicalPath()),
+            )
+        )
+        try:
+            return tObj.transferToAll()
+        except BaseException:
+            pass
+
+
+def is_sender(obj):
+    roles = api.user.get_roles(obj=obj)
+    if "Manager" in roles or "Site Administrator" in roles:
+        return True
+    groups = api.user.get_current().getGroups()
+    for group in groups:
+        if "Senders" in group:
+            return True
+    return False
---
+++ Plone/src/docpool.base/docpool/base/content/dptransferfolder.py
@@ -0,0 +1,308 @@
+from AccessControl import ClassSecurityInfo
+from docpool.base import DocpoolMessageFactory as _
+from docpool.base.content.archiving import IArchiving
+from docpool.base.content.doctype import IDocType
+from docpool.base.content.documentpool import IDocumentPool
+from docpool.base.content.folderbase import FolderBase
+from docpool.base.content.folderbase import IFolderBase
+from docpool.base.marker import IImportingMarker
+from docpool.base.utils import _copyPaste
+from docpool.base.utils import execute_under_special_role
+from docpool.base.utils import queryForObject
+from docpool.base.utils import queryForObjects
+from docpool.elan.config import ELAN_APP
+from logging import getLogger
+from persistent.mapping import PersistentMapping
+from plone import api
+from plone.base.interfaces.siteroot import IPloneSiteRoot
+from plone.dexterity.interfaces import IEditFinishedEvent
+from plone.supermodel import model
+from Products.CMFPlone.utils import log
+from Products.CMFPlone.utils import parent
+from zope import schema
+from zope.component import adapter
+from zope.globalrequest import getRequest
+from zope.interface import implementer
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
+from zope.lifecycleevent.interfaces import IObjectRemovedEvent
+
+
+logger = getLogger(__name__)
+
+
+class IDPTransferFolder(model.Schema, IFolderBase):
+    """ """
+
+    sendingESD = schema.Choice(
+        title=_(
+            "label_dptransferfolder_sendingesd",
+            default="The organisation sending content via this transfer folder",
+        ),
+        description=_("description_dptransferfolder_sendingesd", default=""),
+        required=True,
+        source="docpool.base.vocabularies.DocumentPools",
+    )
+
+    permLevel = schema.Choice(
+        title=_("label_dptransferfolder_permlevel", default="Permission level"),
+        description=_("description_dptransferfolder_permlevel", default=""),
+        required=True,
+        default="read/write",
+        source="docpool.transfers.vocabularies.Permissions",
+    )
+
+    doctypePermissions = schema.Dict(
+        title=_(
+            "label_dptransferfolder_doctypepermissions",
+            default="Permissions by document type",
+        ),
+        value_type=schema.Choice(
+            required=True,
+            source="docpool.transfers.vocabularies.DTPermOptions",
+        ),
+        key_type=schema.TextLine(),
+    )
+
+    unknownDtDefault = schema.Choice(
+        title=_(
+            "label_dptransferfolder_unknowndtdefault",
+            default="Default for unknown document types",
+        ),
+        description=_("description_dptransferfolder_unknowndtdefault", default=""),
+        required=True,
+        default="block",
+        source="docpool.transfers.vocabularies.UnknownOptions",
+    )
+
+    unknownScenDefault = schema.Choice(
+        title=_(
+            "label_dptransferfolder_unknownscendefault",
+            default="Default for unknown scenarios",
+        ),
+        description=_("description_dptransferfolder_unknownscendefault", default=""),
+        required=True,
+        default="block",
+        source="docpool.transfers.vocabularies.UnknownOptions",
+    )
+
+
+@implementer(IDPTransferFolder)
+class DPTransferFolder(FolderBase):
+    """ """
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.doctypePermissions = PersistentMapping()
+
+    # TODO should be indexed
+    def from_to_title(self):
+        from_title = self.getSendingESD().Title()
+        to_title = self.myDocumentPool().Title()
+        return f"{from_title} --> {to_title} ({self.title})"
+
+    def acceptsDT(self, dt_id):
+        """
+        Do I specifically accept this doc type?
+        """
+        perm = self.doctypePermissions.get(dt_id, False)
+        return perm and perm != "block"
+
+    def getMatchingDocumentTypes(self, ids_only=True):
+        """ """
+
+        def doIt():
+            esd = self.getSendingESD()
+            theirDts = esd.myDocumentTypes(ids_only=True)
+            myDts = self.myDocumentTypes()
+            # print theirDts
+            # print myDts
+            if ids_only:
+                return [dt[0] for dt in myDts if dt[0] in theirDts]
+            else:
+                return [dt for dt in myDts if dt[0] in theirDts]
+
+        return execute_under_special_role(self, "Manager", doIt)
+
+    def ensureDocTypeInTarget(self, doctype):
+        """If my document type is unknown in the target ESD, copy it to the target.
+
+        Set it to private state.
+
+        """
+        config = self.myDocumentPool()["config"]["dtypes"]
+        if doctype.id in config:
+            return
+
+        copy_id = _copyPaste(doctype, config)
+        copy_obj = config[copy_id]
+
+        # Set intermediate category
+        copy_obj.doc_extension(ELAN_APP).setCCategory("recent")
+
+        api.content.transition(copy_obj, transition="retract")
+        copy_obj.reindexObject()
+        copy_obj.reindexObjectSecurity()
+        config.reindexObject()
+
+    def getSendingESD(self):
+        """ """
+        esd_uid = self.sendingESD
+        return queryForObject(self, UID=esd_uid)
+
+    def resetSettings(self):
+        """ """
+        created(self, event=None)
+
+    def grantReadAccess(self):
+        """ """
+
+        def grantRead():
+            esd = self.getSendingESD()
+            prefix = esd.myPrefix()
+            esd_members = "%s_Senders" % prefix
+            self.myDocumentPool().manage_setLocalRoles(esd_members, ["Reader"])
+            self.myDocumentPool().reindexObject()
+            self.myDocumentPool().reindexObjectSecurity()
+
+        execute_under_special_role(self, "Manager", grantRead)
+
+    def revokeReadAccess(self):
+        """ """
+
+        def revokeRead():
+            esd = self.getSendingESD()
+            if esd:
+                prefix = esd.myPrefix()
+                esd_members = "%s_Senders" % prefix
+                self.myDocumentPool().manage_delLocalRoles([esd_members])
+                self.myDocumentPool().reindexObject()
+                self.myDocumentPool().reindexObjectSecurity()
+
+        execute_under_special_role(self, "Manager", revokeRead)
+
+    def isArchive(self):
+        return "archive" in self.getPhysicalPath()
+
+    def myDPTransferFolder(self):
+        """ """
+        return self
+
+    def getFirstChild(self):
+        """ """
+        fc = self.getFolderContents()
+        if len(fc) > 0:
+            return fc[0].getObject()
+        else:
+            return None
+
+    def getAllContentObjects(self):
+        """ """
+        return [obj.getObject() for obj in self.getFolderContents()]
+
+    def getDPDocuments(self, **kwargs):
+        """ """
+        args = {"portal_type": "DPDocument"}
+        args.update(kwargs)
+        return [obj.getObject() for obj in self.getFolderContents(args)]
+
+    def getFiles(self, **kwargs):
+        """ """
+        args = {"portal_type": "File"}
+        args.update(kwargs)
+        return [obj.getObject() for obj in self.getFolderContents(args)]
+
+    def getImages(self, **kwargs):
+        """ """
+        args = {"portal_type": "Image"}
+        args.update(kwargs)
+        return [obj.getObject() for obj in self.getFolderContents(args)]
+
+    def getSRModules(self, **kwargs):
+        """ """
+        args = {"portal_type": "SRModule"}
+        args.update(kwargs)
+        return [obj.getObject() for obj in self.getFolderContents(args)]
+
+    def getSituationReports(self, **kwargs):
+        """ """
+        args = {"portal_type": "SituationReport"}
+        args.update(kwargs)
+        return [obj.getObject() for obj in self.getFolderContents(args)]
+
+
+@adapter(IDPTransferFolder, IObjectAddedEvent)
+def created(obj, event=None):
+    # For all document types shared between the two ESDs
+    # set the default to "publish"
+    if IArchiving(obj).is_archive:
+        return
+    if IImportingMarker.providedBy(getRequest()):
+        return
+    log("TransferFolder created: %s" % str(obj))
+    dts = obj.getMatchingDocumentTypes(ids_only=True)
+    obj.doctypePermissions.update(dict.fromkeys(dts, "publish"))
+
+    # Also, if the permissions include read access,
+    # set the local Reader role for the members of
+    # the sending ESD
+    if obj.permLevel == "read/write":
+        obj.grantReadAccess()
+
+
+@adapter(IDPTransferFolder, IEditFinishedEvent)
+def updated(obj, event=None):
+    # Actually, a transfer folder should never allow a change of ESD.
+    # But the permission level could have been changed. So we adapt
+    # the read permissions for the sending ESD accordingly.
+    log("TransferFolder updated: %s" % str(obj))
+
+    if not IArchiving(obj).is_archive():
+        if obj.permLevel == "read/write":
+            obj.grantReadAccess()
+        else:
+            obj.revokeReadAccess()
+
+
+@adapter(IDPTransferFolder, IObjectRemovedEvent)
+def deleted(obj, event=None):
+    log("TransferFolder deleted: %s" % str(obj))
+    if not IArchiving(obj).is_archive:
+        if IPloneSiteRoot.providedBy(event.object) or IDocumentPool.providedBy(
+            event.object
+        ):
+            # do not modify content from the site or docpool that will be deleted
+            return
+        # Revoke any read access
+        obj.revokeReadAccess()
+
+
+def transfer_folders_for(obj):
+    try:
+        esd = obj.myDocumentPool()
+    except AttributeError:
+        return []
+
+    brains = queryForObjects(
+        esd, path=esd.dpSearchPath(), object_provides=IDPTransferFolder.__identifier__
+    )
+    return [brain.getObject() for brain in brains]
+
+
+@adapter(IDocType, IObjectAddedEvent)
+def doctype_added(obj, event=None):
+    if IImportingMarker.providedBy(getRequest()):
+        return
+    dt_id = obj.getId()
+    for tf in transfer_folders_for(obj):
+        tf.doctypePermissions.setdefault(dt_id, "publish")
+
+
+@adapter(IDocType, IObjectRemovedEvent)
+def doctype_will_be_removed(obj, event=None):
+    if event is None:
+        return
+    dt_id = obj.getId()
+    for tf in transfer_folders_for(event.oldParent):
+        tf.doctypePermissions.pop(dt_id, None)
