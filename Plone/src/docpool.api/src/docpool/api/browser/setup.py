a46e8879f21d2875cf847d7777510601b2fd5648
Philip Bauer <bauer@starzel.de>
2022-09-22 18:10:38 +0200

    re-enable nav tests and eea feature

--- Plone/src/docpool.api/src/docpool/api/browser/setup.py
+++ Plone/src/docpool.api/src/docpool/api/browser/setup.py
@@ -652,8 +652,7 @@ class DocpoolSetup(BrowserView):
             local_behaviors=["rei"],
             exclude_from_nav=False,
         )
-        # TODO: Enable facetednav when eea.facetednavigation works in Plone 6.
-        # _configure_faceted_view(search, import_file_path, "faceted-table-items")
+        _configure_faceted_view(search, import_file_path, "faceted-table-items")
 
         # Why we need to set to empty?
         # Todo: https://redmine-koala.bfs.de/issues/3951
