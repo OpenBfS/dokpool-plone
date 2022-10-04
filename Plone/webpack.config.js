0f81231a8c368cd9fee1248c7f3c9ed138b86d49
Steffen Lindner <mail@steffen-lindner.de>
2022-10-04 16:48:07 +0200

    Add loader to let webpack handle all scss.

--- Plone/webpack.config.js
+++ Plone/webpack.config.js
@@ -12,5 +12,25 @@ module.exports = [
       path: path.resolve(__dirname, "src/docpool.theme/docpool/theme/static/"),
       filename: "docpool.min.js",
     },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            { loader: "style-loader" },
+            // Translates CSS into CommonJS
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+              },
+            },
+            // Compiles Sass to CSS
+            { loader: "sass-loader" },
+          ],
+        },
+      ],
+    },
   },
 ];
