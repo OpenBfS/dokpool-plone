fa6b7953b0c410020e2052cab536d518691df2ef
Steffen Lindner <mail@steffen-lindner.de>
2022-10-04 16:53:18 +0200

    Add anonymous styling

--- Plone/src/docpool.theme/docpool/theme/resources/docpool.js
+++ Plone/src/docpool.theme/docpool/theme/resources/docpool.js
@@ -4,3 +4,13 @@ import { makePopUp } from "./window_functions";
 // Global functions needed in window
 window.go_to = go_to;
 window.makePopUp = makePopUp;
+
+// Logged-In users
+if (document.querySelector(".userrole-anonymous") === null) {
+  import("./theme.scss");
+}
+
+// Anonymous users
+if (document.querySelector(".userrole-anonymous") !== null) {
+  import("./docpool_styles/anonymous.scss");
+}
