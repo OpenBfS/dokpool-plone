58dbe0c339e05a40b356fff3beecfb8ab14490cc
Steffen Lindner <mail@steffen-lindner.de>
2022-11-23 16:12:37 +0100

    Add elan.journal styles

--- Plone/webpack.config.js
+++ Plone/webpack.config.js
@@ -2,36 +2,37 @@ process.traceDeprecation = true;
 const path = require("path");
 
 module.exports = [
-  {
-    // Main bundle
-    entry: "./src/docpool.theme/docpool/theme/resources/docpool.js",
-    // Disable for debugging and development
-    optimization: {
-      minimize: true,
-    },
-    output: {
-      path: path.resolve(__dirname, "src/docpool.theme/docpool/theme/static/"),
-      filename: "docpool.min.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            { loader: "style-loader" },
-            // Translates CSS into CommonJS
-            {
-              loader: "css-loader",
-              options: {
-                url: false,
-              },
-            },
-            // Compiles Sass to CSS
-            { loader: "sass-loader" },
-          ],
+    {
+        // Main bundle
+        entry: ["./src/docpool.theme/docpool/theme/resources/docpool.js",
+            "./src/elan.journal/elan/journal/resources/default.js"],
+        // Disable for debugging and development
+        optimization: {
+            minimize: true,
         },
-      ],
+        output: {
+            path: path.resolve(__dirname, "src/docpool.theme/docpool/theme/static/"),
+            filename: "docpool.min.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        {loader: "style-loader"},
+                        // Translates CSS into CommonJS
+                        {
+                            loader: "css-loader",
+                            options: {
+                                url: false,
+                            },
+                        },
+                        // Compiles Sass to CSS
+                        {loader: "sass-loader"},
+                    ],
+                },
+            ],
+        },
     },
-  },
 ];
