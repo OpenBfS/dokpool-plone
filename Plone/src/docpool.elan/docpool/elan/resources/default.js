3c63251435fdba7f0de1dcce52eb9cccc70897e5
Steffen Lindner <mail@steffen-lindner.de>
2023-05-31 16:43:44 +0200

    Bring back ticker (marquee)

--- Plone/src/docpool.elan/docpool/elan/resources/default.js
+++ Plone/src/docpool.elan/docpool/elan/resources/default.js
@@ -1,3 +1,14 @@
 // docpool.elan js/scss entry
 
+import $ from "jquery";
+import "jquery.marquee";
 import("./styles.scss");
+
+$(function () {
+  $(".marquee").marquee({
+    pauseOnHover: true,
+    duration: 10000,
+    //gap in pixels between the tickers
+    gap: 50,
+  });
+});
