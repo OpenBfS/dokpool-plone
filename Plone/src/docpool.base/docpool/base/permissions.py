c9e37236cbf235ff1107ec463f9af48ec4fd8592
Philip Bauer <bauer@starzel.de>
2024-02-02 16:20:29 +0100

    Add Site Administrator Role to Sharing View (#5432)

---
+++ Plone/src/docpool.base/docpool/base/permissions.py
@@ -0,0 +1,20 @@
+from AccessControl import ModuleSecurityInfo
+from docpool.base import _
+from plone.app.workflow.interfaces import ISharingPageRole
+from Products.CMFCore.permissions import setDefaultRoles
+from zope.interface import implementer
+
+
+security = ModuleSecurityInfo("plone.app.workflow.permissions")
+
+
+security.declarePublic("DelegateSiteAdminRole")
+DelegateSiteAdminRole = "Sharing page: Delegate Site Administrator role"
+setDefaultRoles(DelegateSiteAdminRole, ("Manager", "Site Administrator"))
+
+
+@implementer(ISharingPageRole)
+class SiteAdminRole:
+    title = _("title_can_manage_site", default="Can manage site")
+    required_permission = DelegateSiteAdminRole
+    required_interface = None
