e510e2e710f764347c7c6785ff925a0634061fbb
Philip Bauer <bauer@starzel.de>
2024-02-02 16:19:25 +0100

    Show all roles in custom sharing page for dokpools (#5432)

--- Plone/src/docpool.base/docpool/base/browser/sharing.py
+++ Plone/src/docpool.base/docpool/base/browser/sharing.py
@@ -8,13 +8,7 @@ from zope.i18n import translate
 
 
 class SharingView(OSV):
-    @memoize
-    def roles(self):
-        """
-        Reduce the available roles for sharing
-        """
-        pairs = OSV.roles(self)
-        return [p for p in pairs if p["id"] == "Reader"]
+    """Customized sharing view for docpools to return all groups and users"""
 
     def _principal_search_results(
         self,
