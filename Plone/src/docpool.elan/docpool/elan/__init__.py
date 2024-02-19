a21378fdd87551727e1e14d7329d81d254b81709
Philip Bauer <bauer@starzel.de>
2024-02-19 14:40:23 +0100

    fix issues that appear with a pip-based installation

--- Plone/src/docpool.elan/docpool/elan/__init__.py
+++ Plone/src/docpool.elan/docpool/elan/__init__.py
@@ -1,11 +1,11 @@
 from AccessControl import allow_module
-from docpool.elan import monkey  # noqa: F401
 from zope.i18nmessageid import MessageFactory
 
 
 DocpoolMessageFactory = MessageFactory("docpool.elan")
 
 from docpool.elan import appregistration  # noqa: F401
+from docpool.elan import monkey  # noqa: F401
 
 
 allow_module("docpool.elan.config")
