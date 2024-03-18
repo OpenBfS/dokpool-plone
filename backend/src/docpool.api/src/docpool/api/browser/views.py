41f913753d08cffda27235c12de55dbbbc1afbae
Philip Bauer <bauer@starzel.de>
2024-03-18 18:03:02 +0100

    add view @@json for debugging (#5551)

---
+++ backend/src/docpool.api/src/docpool/api/browser/views.py
@@ -0,0 +1,19 @@
+from Acquisition import aq_base
+from plone.restapi.interfaces import ISerializeToJson
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.Five.browser import BrowserView
+from zope.component import getMultiAdapter
+
+import json
+
+
+class JSONView(BrowserView):
+
+    def __call__(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        serializer = getMultiAdapter((self.context, self.request), ISerializeToJson)
+        if IPloneSiteRoot.providedBy(self.context):
+            item = serializer()
+        elif getattr(aq_base(self.context), "isPrincipiaFolderish", False):
+            item = serializer(include_items=False)
+        return json.dumps(item, indent=4, sort_keys=True)
