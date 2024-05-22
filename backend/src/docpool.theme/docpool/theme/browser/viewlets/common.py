d1851aee253930f10baaa0afcff4daf2bd95cd4e
Steffen Lindner <mail@steffen-lindner.de>
2024-05-22 16:21:24 +0200

    Read commit hash from pre created file - fixes #5574

--- backend/src/docpool.theme/docpool/theme/browser/viewlets/common.py
+++ backend/src/docpool.theme/docpool/theme/browser/viewlets/common.py
@@ -1,9 +1,9 @@
 from AccessControl.SecurityInfo import allow_class
+from App.config import getConfiguration
 from docpool.base.appregistry import APP_REGISTRY
 from plone.app.layout.viewlets.common import LogoViewlet
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from subprocess import check_output
 
 import os
 import plone.api as api
@@ -36,12 +36,33 @@ class LogoDocpoolViewlet(LogoViewlet):
         if api.portal.get_registry_record(name="docpool.show_debug_info"):
             return True
 
+    def read_git_version_file(self, filename):
+        # Get the path to the pid file :)
+        varbase = os.path.dirname(getConfiguration().pid_filename)
+        project_root = os.path.abspath(os.path.join(varbase, ".."))
+        file_path = os.path.join(project_root, filename)
+        if not os.path.isfile(file_path):
+            return
+
+        with open(file_path) as file:
+            content = file.read()
+            if content:
+                return content
+
     def get_git_rev(self):
         # Git Revision
+
+        # If run inside docker with existing version file
+        version_in_file = self.read_git_version_file("GIT_COMMIT")
+        if version_in_file:
+            return version_in_file
+
+        # If run on the review server
         commit_hash = os.getenv("GIT_COMMIT")
         if commit_hash:
             return commit_hash
 
+        # If run local
         result = subprocess.run(
             shlex.split("git rev-parse --short HEAD"),
             capture_output=True,
