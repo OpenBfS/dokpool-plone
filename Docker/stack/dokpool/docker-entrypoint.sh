1b0cf12df5656f0d5998f75aef38b7c1546c5777
Steffen Lindner <mail@steffen-lindner.de>
2024-06-21 17:35:41 +0200

    Add dokpool init zeo

--- Docker/stack/dokpool/docker-entrypoint.sh
+++ Docker/stack/dokpool/docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-sed -i "s/address 8081/address $DOKPOOL_INSTANCE_PORT/g" /opt/bfs/dokpool/Plone/parts/instance1/etc/zope.conf
-sed -i "/ELANENGINE/c\ELANENGINE postgres://${TRANSFER_DB_USER}:${TRANSFER_DB_PASS}@${TRANSFER_DB_HOST}:${TRANSFER_DB_PORT}/${TRANSFER_DB_NAME}" /opt/bfs/dokpool/Plone/parts/instance1/etc/zope.conf
-sed -i "/dsn/c\dsn dbname=${DB_NAME} user=${DB_USER} host=${DB_HOST} password=${DB_PASS} port=${DB_PORT}" /opt/bfs/dokpool/Plone/parts/instance1/etc/zope.conf &
+set -e
+
+zope_conf="/opt/bfs/dokpool/Plone/parts/instance1/etc/zope.conf"
+
+sed -i "s/address 8081/address $DOKPOOL_INSTANCE_PORT/g" "$zope_conf"
+sed -i "/ELANENGINE/c\ELANENGINE postgres://${TRANSFER_DB_USER}:${TRANSFER_DB_PASS}@${TRANSFER_DB_HOST}:${TRANSFER_DB_PORT}/${TRANSFER_DB_NAME}" "$zope_conf"
+sed -i "/dsn/c\dsn dbname=${DB_NAME} user=${DB_USER} host=${DB_HOST} password=${DB_PASS} port=${DB_PORT}" "$zope_conf" &
+if [ -n "${RELSTORAGE_SKIP_CREATE_SCHEMA}" ]; then
+  if ! grep -q "create-schema" "$zope_conf"; then
+      sed -i "/shared-blob-dir/a\create-schema false" "$zope_conf" &
+  fi
+fi
 
 exec /opt/bfs/dokpool/Plone/bin/instance1 console
 
