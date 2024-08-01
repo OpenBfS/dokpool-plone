585c20e0fdfc522aee09c6bf579f12f2298a1304
Philip Bauer <bauer@starzel.de>
2024-08-01 10:01:55 +0200

    refactor indexers of local behaviors to work when reindexing content and clear & rebuild (#5700)

--- backend/src/docpool.base/docpool/base/content/indexer.py
+++ backend/src/docpool.base/docpool/base/content/indexer.py
@@ -1,31 +1,29 @@
 from docpool.base.content.documentpool import IDocumentPool
 from docpool.base.content.infodocument import IInfoDocument
 from docpool.base.localbehavior.localbehavior import ILocalBehaviorSupport
-from docpool.base.localbehavior.localbehavior import ILocalBehaviorSupporting
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
 from Products.CMFPlone.utils import base_hasattr
 
 
 @indexer(IDocumentPool)
-def docpool_apps_indexer(obj):
+def docpool_apps_supported(obj):
     try:
         return obj.supportedApps
     except BaseException:
         pass
 
 
-@indexer(ILocalBehaviorSupporting)
-def local_behaviors_apps_indexer(obj):
+@indexer(IDexterityContent)
+def apps_supported(obj):
+    # Registering a indexer more specific for ILocalBehaviorSupporting will fail
+    # during clear & rebuild of the catalog. The indexer-lookup uses a object-based
+    # cache in get_assignable that sometimes not yet finds DexterityLocalBehaviorAssignable.
+    # So we register the indexer more generally and check for ILocalBehaviorSupport only here.
     try:
         return ILocalBehaviorSupport(obj).local_behaviors
     except TypeError:
-        # This only happends sometimes during test-setup.
         pass
-
-
-@indexer(IDexterityContent)
-def base_apps_indexer(obj):
     if base_hasattr(obj, "APP"):
         return [obj.APP]
 
