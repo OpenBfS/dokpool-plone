c930cbc8cb20ca6796bf812e8fb6fe503f924dba
Thomas Lotze <lotze@starzel.de>
2025-03-12 14:47:50 +0100

    checkpoint: use react

--- backend/src/docpool.ui/docpool/ui/resources/default.js
+++ backend/src/docpool.ui/docpool/ui/resources/default.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import React from "react";
+import { render } from "react-dom";
+
+const Listing = () => {
+  console.log("test");
+  return <div>test</div>;
+};
+
+const el = document.getElementById("docpool-listing");
+console.log(el);
+render(<Listing />, el);
