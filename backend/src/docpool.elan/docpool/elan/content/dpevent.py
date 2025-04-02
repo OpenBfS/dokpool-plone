2558b5220fefeee67b4662d3d3cba8344232bef5
Philip Bauer <bauer@starzel.de>
2025-04-02 17:53:15 +0200

    WIP: Improve archiving events (#5998)

--- backend/src/docpool.elan/docpool/elan/content/dpevent.py
+++ backend/src/docpool.elan/docpool/elan/content/dpevent.py
@@ -1,22 +1,12 @@
 from AccessControl import ClassSecurityInfo
-from DateTime import DateTime
-from docpool.base.config import TRANSFERS_APP
 from docpool.base.content.archiving import IArchiving
 from docpool.base.content.contentbase import ContentBase
 from docpool.base.content.contentbase import IContentBase
 from docpool.base.content.documentpool import IDocumentPool
-from docpool.base.localbehavior.localbehavior import ILocalBehaviorSupport
 from docpool.base.marker import IImportingMarker
-from docpool.config.local.base import navSettings
-from docpool.config.local.elan import ARCHIVESTRUCTURE
-from docpool.config.local.transfers import TRANSFER_AREA
-from docpool.config.utils import createPloneObjects
-from docpool.config.utils import ploneId
 from docpool.elan import DocpoolMessageFactory as _
-from docpool.elan.behaviors.elandocument import IELANDocument
 from docpool.elan.config import ELAN_APP
 from docpool.elan.utils import get_global_scenario_selection
-from elan.journal.adapters import IJournalEntryContainer
 from logging import getLogger
 from plone import api
 from plone.autoform import directives
@@ -24,7 +14,6 @@ from plone.base.i18nl10n import utranslate
 from plone.base.interfaces.siteroot import IPloneSiteRoot
 from plone.base.utils import safe_text
 from plone.dexterity.content import Container
-from plone.protect.interfaces import IDisableCSRFProtection
 from Products.CMFCore.interfaces import IActionSucceededEvent
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import log
@@ -35,10 +24,10 @@ from z3c.form.interfaces import IEditForm
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope import schema
+from zope.annotation.interfaces import IAnnotations
 from zope.component import adapter
 from zope.component import getUtility
 from zope.globalrequest import getRequest
-from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface import Invalid
@@ -49,9 +38,9 @@ from zope.schema.interfaces import IVocabularyFactory
 
 import datetime
 import json
-import transaction
 
 
+ARCHIVING_KEY = "docpool.elan.archiving"
 logger = getLogger(__name__)
 
 
@@ -247,330 +236,11 @@ class DPEvent(Container, ContentBase):
         """
         return utranslate("docpool.elan", "snapshot_confirm_msg", context=self)
 
-    security.declareProtected("Modify portal content", "archiveAndClose")
+    def get_archiving_info(self):
+        return IAnnotations(self).get(ARCHIVING_KEY, {})
 
-    def archiveAndClose(self, REQUEST):
-        """
-        Saves all content for this scenario to an archive, deletes the original content,
-        and sets the scenario to state "closed".
-
-        This can take a long time since associated DPDocuments can have many attechments
-        and all need to be copied or moved and reindexed. This is why we make savepoints.
-        """
-        alsoProvides(REQUEST, IDisableCSRFProtection)
-
-        # 1. Disable scenario to prevent new data from being added for it
-        global_scenarios = get_global_scenario_selection()
-        global_scenarios[self.UID()] = "closed"
-        self.Status = "closed"
-        transaction.savepoint(optimistic=True)
-
-        # 2. Create Archive
-        archive = self._createArchive()
-        archive_contentarea = archive.content
-        logger.info("Archiving DPEvent %s to %s", self.title, archive.absolute_url())
-        contentarea = self.content
-        contentarea_path = "/".join(contentarea.getPhysicalPath())
-
-        # 3. Move or Copy related DPDocuments
-        brains = self._getDocumentsForScenario(path=contentarea_path)
-        total = len(brains)
-        logger.info(
-            "Archiving %s items associated with DPEvent %s. This may take a while...",
-            total,
-            self.absolute_url(),
-        )
-        for index, brain in enumerate(brains, start=1):
-            obj = brain.getObject()
-            target_folder = self._ensureTargetFolder(obj, archive_contentarea)
-            if self.can_move(obj):
-                self._move_to_archive(target_folder, obj)
-            else:
-                self._copy_to_archive(target_folder, obj)
-
-            if not index % 10:
-                logger.info("Archived %s of %s documents...", index, total)
-                transaction.savepoint(optimistic=True)
-
-        # 4. Move DPEvent into archive and redirect to it
-        archived_event = api.content.move(self, target=archive)
-        archived_event.reindexObject()
-        # local roles were set when adding items to the archive but reindexing was deferred.
-        archive_contentarea.reindexObjectSecurity()
-        logger.info("Finished archiving DPEvent %s", archived_event.title)
-        api.portal.show_message(_("Scenario archived"), REQUEST)
-        return REQUEST.response.redirect(archived_event.absolute_url())
-
-    security.declareProtected("Modify portal content", "snapshot")
-
-    def snapshot(self, REQUEST=None):
-        """
-        Similar to archiveAndClose but leave the old event as is.
-        Previously this was two seperate actions snapshot & purge. But if content would be
-        added between snapshop and purge that would be lost forever.
-
-        * Create Archive for event but leave Event unchanged
-        * Copy Journals and Event to Archive
-        * Purge Journals
-        * Purge Event (move content to archive unless used by other apps or events)
-        """
-        alsoProvides(REQUEST, IDisableCSRFProtection)
-
-        # 1. Create Archive
-        archive = self._createArchive()
-        archive_contentarea = archive.content
-        logger.info(
-            "Create snapshot of DPEvent %s in %s", self.title, archive.absolute_url()
-        )
-
-        # 2. Move or Copy related DPDocuments (previously done by purge)
-        contentarea = self.content
-        contentarea_path = "/".join(contentarea.getPhysicalPath())
-        brains = self._getDocumentsForScenario(path=contentarea_path)
-        total = len(brains)
-        logger.info(
-            "Archiving %s items associated with DPEvent %s. This may take a while...",
-            total,
-            self.absolute_url(),
-        )
-        for index, brain in enumerate(brains, start=1):
-            obj = brain.getObject()
-            target_folder = self._ensureTargetFolder(obj, archive_contentarea)
-            if self.can_move(obj):
-                self._move_to_archive(target_folder, obj)
-            else:
-                self._copy_to_archive(target_folder, obj)
-
-            if not index % 10:
-                logger.info("Archived %s of %s documents...", index, total)
-                transaction.savepoint(optimistic=True)
-
-        # 3. Copy DPEvent and Journals into snapshot
-        copied_event = api.content.copy(self, target=archive)
-        copied_event.Status = "closed"
-        copied_event.reindexObject()
-
-        # 4. Empty current Journals and copy local roles
-        for journal in self.contentValues({"portal_type": "Journal"}):
-            adapter = IJournalEntryContainer(journal)
-            for id, update in enumerate(adapter):
-                adapter.delete(id)
-            # Copy local roles to journals
-            copied_journal = copied_event[journal.id]
-            copied_journal.__ac_local_roles__ = journal.__ac_local_roles__
-            copied_journal._p_changed = True
-
-        # local roles were set when adding items to the archive but reindexing was deferred.
-        archive_contentarea.reindexObjectSecurity()
-
-        logger.info("Finished snapshot of DPEvent %s", self.title)
-        api.portal.show_message(_("Created snapshot of event."), REQUEST)
-        return REQUEST.response.redirect(copied_event.absolute_url())
-
-    def _ensureTargetFolder(self, obj, target):
-        """
-        Make sure that a personal or group folder with proper permissions
-        exists for this document in the archive.
-        """
-        path = obj.getPhysicalPath()
-
-        # 1. check whether this is a personal or a group document
-        isGroup = "Groups" in path
-        isTransfer = "Transfers" in path
-        isMember = "Members" in path
-        if isGroup:
-            target = target.Groups
-            container = self.content.Groups
-            foldertype = "GroupFolder"
-        elif isMember:
-            target = target.Members
-            container = self.content.Members
-            foldertype = "UserFolder"
-        elif isTransfer:
-            target = target.Transfers
-            container = self.content.Transfers
-            foldertype = "DPTransferFolder"
-
-        # 2. check for which user / group
-        if isGroup or isTransfer or isMember:
-            foldername = path[5]
-        else:
-            foldername = path[4]
-
-        # 3. check for corresponding folder
-        if foldername in target:
-            if not isTransfer:
-                mtool = api.portal.get_tool("portal_membership")
-                mtool.setLocalRoles(
-                    target[foldername], [foldername], "Owner", reindex=False
-                )
-            return target[foldername]
-
-        # 4. if it doesn't exist: create it
-        old_parent = container.get(foldername)
-        new = api.content.create(
-            container=target,
-            type=foldertype,
-            id=foldername,
-            title=old_parent.title,
-        )
-
-        # 5. and copy the local roles
-        if not isTransfer:
-            localroles = getattr(old_parent, "__ac_local_roles__", [])
-            if localroles:
-                for userid in localroles:
-                    new.manage_setLocalRoles(userid=userid, roles=localroles[userid])
-            block = getattr(old_parent, "__ac_local_roles_block__", False)
-            if block:
-                new.__ac_local_roles_block__ = block
-
-        return new
-
-    def can_move(self, obj):
-        try:
-            scns = IELANDocument(obj).scenarios
-        except BaseException:
-            # Object could have lost its ELAN behavior but that means we can
-            # potentially delete it
-            scns = ["dummy"]
-        # Ignore duplicates!
-        apps = set(ILocalBehaviorSupport(obj).local_behaviors)
-        if len(scns) == 1 and len(apps) == 1:
-            return True
-        return False
-
-    def _move_to_archive(self, target_folder_obj, obj):
-        logger.info(
-            "Moving %s with %s attachments to %s",
-            obj.absolute_url(),
-            len(obj.keys()),
-            target_folder_obj.absolute_url(),
-        )
-
-        mdate = obj.modified()
-        transfer_events = obj.doc_extension(TRANSFERS_APP).transferEvents()
-        old_state = api.content.get_state(obj)
-        moved_obj = api.content.move(obj, target_folder_obj)
-        new_state = api.content.get_state(moved_obj)
-
-        if old_state != new_state:
-            # We directly transition for speed
-            wftool = api.portal.get_tool("portal_workflow")
-            if old_state == "published" and new_state in ["private", "pending"]:
-                wftool.doActionFor(moved_obj, "publish")
-            elif old_state == "pending" and new_state in ["private"]:
-                wftool.doActionFor(moved_obj, "submit")
-            elif old_state == "pending" and new_state in ["published"]:
-                wftool.doActionFor(moved_obj, "retract")
-            else:
-                # See https://redmine-koala.bfs.de/issues/5007
-                api.content.transition(moved_obj, to_state=old_state)
-
-        # Now do some repairs
-        moved_obj.scenarios = []
-        moved_obj.setModificationDate(mdate)
-        # transferLog for archived items needs to be a string
-        moved_obj.transferLog = str(transfer_events)
-        moved_obj.reindexObject(idxs=["modified", "review_state", "scenarios"])
-
-    def _copy_to_archive(self, target_folder_obj, obj):
-        logger.info(
-            "Copying %s with %s attachments to %s",
-            obj.absolute_url(),
-            len(obj.keys()),
-            target_folder_obj.absolute_url(),
-        )
-
-        copied_obj = api.content.copy(obj, target_folder_obj)
-
-        # Now do some repairs
-        mdate = obj.modified()
-        copied_obj.scenarios = []
-
-        old_state = api.content.get_state(obj)
-        new_state = api.content.get_state(copied_obj)
-
-        if old_state != new_state:
-            # We directly transition for speed
-            wftool = api.portal.get_tool("portal_workflow")
-            if old_state == "published" and new_state in ["private", "pending"]:
-                wftool.doActionFor(copied_obj, "publish")
-            elif old_state == "pending" and new_state in ["private"]:
-                wftool.doActionFor(copied_obj, "submit")
-            elif old_state == "pending" and new_state in ["published"]:
-                wftool.doActionFor(copied_obj, "retract")
-            else:
-                # See https://redmine-koala.bfs.de/issues/5007
-                api.content.transition(copied_obj, to_state=old_state)
-
-        copied_obj.setModificationDate(mdate)
-        # transferLog for archived items needs to be a string
-        events = obj.doc_extension(TRANSFERS_APP).transferEvents()
-        copied_obj.transferLog = str(events)
-        copied_obj.reindexObject(idxs=["modified", "review_state", "scenarios"])
-
-        # Cleanup original DPDocument
-        # 1. Remove current scenario
-        scns = IELANDocument(obj).scenarios
-        # Drop duplicates
-        scns = list(set(scns))
-        scns.remove(self.UID())
-        obj.scenarios = scns
-
-        # 2. Remove elan behavior if there are no other events but other behaviors
-        if not scns:
-            apps = ILocalBehaviorSupport(obj).local_behaviors
-            if len(apps) > 1:
-                # There are others --> only remove ELAN behavior
-                try:
-                    apps.remove(ELAN_APP)
-                    ILocalBehaviorSupport(obj).local_behaviors = list(set(apps))
-                except Exception as e:
-                    log_exc(e)
-        obj.reindexObject(idxs=["apps_supported", "scenarios"])
-
-    def _getDocumentsForScenario(self, **kwargs):
-        """
-        Collects all DPDocuments for the current scenario
-        :return: list of brains
-        """
-        #        args = {'object_provides':IDPDocument.__identifier__, 'scenarios': self.getId()}
-        args = {"portal_type": "DPDocument", "scenarios": self.UID()}
-        args.update(kwargs)
-        return api.content.find(**args)
-
-    def _createArchive(self):
-        """
-        We create an archive object. Into it, we copy the complete ESD hierarchy.
-        We also create two folders "Members" and "Groups", which will hold all the
-        documents for the scenario.
-        """
-        archive = self.archive  # Acquire root for archives
-        esd = self.esd  # Acquire esd root
-        now = self.toLocalizedTime(DateTime(), long_format=1)
-        # create the archive root
-        arc = api.content.create(
-            container=archive,
-            type="ELANArchive",
-            id=ploneId(self, f"{self.id}_{now}"),
-            title=f"{self.title} {now}",
-        )
-        arc.description = self.description
-        # create the document folders
-        createPloneObjects(arc, ARCHIVESTRUCTURE)
-        createPloneObjects(arc.content, TRANSFER_AREA)
-
-        navSettings(arc)
-
-        # copy the ESD folders
-        for brain in esd.getFolderContents(
-            {"portal_type": ["ELANSection", "ELANDocCollection"]}
-        ):
-            api.content.copy(brain.getObject(), arc.esd)
-        arc.esd.setDefaultPage("overview")
-
-        return arc
+    def set_archiving_info(self, info):
+        IAnnotations(self)[ARCHIVING_KEY] = info
 
     def selectGlobally(self):
         global_scenarios = get_global_scenario_selection()
