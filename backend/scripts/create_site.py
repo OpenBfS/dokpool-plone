7f2581214abf37df6c74fc4765a7d821a11e1a18
Philip Bauer <bauer@starzel.de>
2025-04-29 16:17:05 +0200

    Milestone: Update to use latest cookieplone-templates (#6012)

--- backend/scripts/create_site.py
+++ backend/scripts/create_site.py
@@ -1,13 +1,11 @@
 from AccessControl.SecurityManagement import newSecurityManager
-from pathlib import Path
-from plone.distribution.api import site as site_api
+from Products.CMFPlone.factory import _DEFAULT_PROFILE
+from Products.CMFPlone.factory import addPloneSite
 from Testing.makerequest import makerequest
 
-import json
 import logging
 import os
 import transaction
-
 
 logging.basicConfig(format="%(message)s")
 
@@ -20,8 +18,6 @@ for logger_name in [
 
 logger = logging.getLogger("Plone Site Creation")
 logger.setLevel(logging.DEBUG)
-
-SCRIPT_DIR = Path().cwd() / "scripts"
 
 truthy = frozenset(("t", "true", "y", "yes", "on", "1"))
 
@@ -38,51 +34,41 @@ def asbool(s):
     return s.lower() in truthy
 
 
+DELETE_EXISTING = asbool(os.getenv("DELETE_EXISTING"))
+EXAMPLE_CONTENT = asbool(
+    os.getenv("EXAMPLE_CONTENT", "1")
+)  # Create example content by default
+
 app = makerequest(globals()["app"])
 
 request = app.REQUEST
+
+# ifaces = [IBrowserLayer]
+# for iface in directlyProvidedBy(request):
+#     ifaces.append(iface)
+
+# directlyProvides(request, *ifaces)
 
 admin = app.acl_users.getUserById("admin")
 admin = admin.__of__(app.acl_users)
 newSecurityManager(None, admin)
 
+site_id = "Plone"
+payload = {
+    "title": "Dokpool Demo",
+    "profile_id": _DEFAULT_PROFILE,
+    "distribution_name": "docpooldemo",
+    "setup_content": True,
+    "default_language": "de",
+    "portal_timezone": "Europe/Berlin",
+}
 
-def get_answers_file() -> Path:
-    filename = f"{ANSWERS}.json"
-    return SCRIPT_DIR / filename
-
-
-def parse_answers(answers_file: Path, site_id: str = "") -> dict:
-    answers = json.loads(answers_file.read_text())
-    if "distribution" not in answers:
-        # This is a bug in plone.distribution and should be fixed there
-        answers["distribution"] = DISTRIBUTION
-    if site_id:
-        answers["site_id"] = site_id
-    return answers
-
-
-# VARS
-DISTRIBUTION = os.getenv("DISTRIBUTION", "docpooldemo")
-SITE_ID = os.getenv("SITE_ID")  # if set, this overrides the value in ANSWERS
-ANSWERS = os.getenv("ANSWERS", "default")
-DELETE_EXISTING = asbool(os.getenv("DELETE_EXISTING"))
-
-# Load site creation parameters
-answers_file = get_answers_file()
-answers = parse_answers(answers_file, SITE_ID)
-site_id = answers["site_id"]
-
-
-logger.info(f"Creating a new Plone site  @ {site_id}")
-logger.info(f" - Using the {DISTRIBUTION} distribution and answers from {answers_file}")
-
+logger.info(f"Creating a new Plone site {site_id}")
 
 if site_id in app.objectIds() and DELETE_EXISTING:
     app.manage_delObjects([site_id])
     transaction.commit()
     app._p_jar.sync()
-    logger.info(f" - Deleted existing site with id {site_id}")
 elif site_id in app.objectIds() and not DELETE_EXISTING:
     logger.info(
         f" - Stopping site creation, as there is already a site with id {site_id} at the instance. "
@@ -90,9 +76,7 @@ elif site_id in app.objectIds() and not DELETE_EXISTING:
     )
 
 if site_id not in app.objectIds():
-    site = site_api._create_site(
-        context=app, distribution_name=DISTRIBUTION, answers=answers
-    )
+    site = addPloneSite(app, site_id, **payload)
     transaction.commit()
     app._p_jar.sync()
     logger.info(" - Site created!")
