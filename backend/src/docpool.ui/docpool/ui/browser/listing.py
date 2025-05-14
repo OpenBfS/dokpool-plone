734fcb5bebfb04cbd84e05649545627caac0e0d6
Steffen Lindner <mail@steffen-lindner.de>
2025-05-14 11:36:37 +0200

    Async fetch

--- backend/src/docpool.ui/docpool/ui/browser/listing.py
+++ backend/src/docpool.ui/docpool/ui/browser/listing.py
@@ -1,5 +1,5 @@
 from Products.Five.browser import BrowserView
-
+import time
 import json
 
 
@@ -7,6 +7,7 @@ class Listing(BrowserView):
     """Example view called from template"""
 
     def __call__(self, title=None):
+        # Do we fetch form catalog? UUIDs?
         self.items = json.dumps(("1", "2", "3"))
         return self.index()
 
@@ -16,4 +17,10 @@ class Item(BrowserView):
 
     def __call__(self, title=None):
         self.title = title
+        if title == '1':
+            time.sleep(2)
+        if title == '2':
+            time.sleep(5)
+        if title == '3':
+            time.sleep(10)
         return self.index()
--- backend/src/docpool.ui/docpool/ui/resources/default.js
+++ backend/src/docpool.ui/docpool/ui/resources/default.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import "./base.scss";
 
 const Listing = ({ items }) => {
   const [data, setData] = useState([]);
@@ -10,14 +11,27 @@ const Listing = ({ items }) => {
     const fetchData = async () => {
       const urls = json_items.map((item) => `http://localhost:8080/Plone/listing-item?title=${item}`);
       console.log(urls);
-      try {
-        const responses = await Promise.all(urls.map((url) => fetch(url)));
-        const html = await Promise.all(responses.map((res) => res.text()));
-        console.log(html);
-        setData(html);
-      } catch (error) {
-        console.error("Fehler beim Laden der Daten", error);
-      }
+
+      // Empty the data array
+      setData(new Array(urls.length).fill(null));
+
+      // Process each URL
+      urls.forEach(async (url, index) => {
+        try {
+          const response = await fetch(url);
+          const html = await response.text();
+          console.log(`Loaded item ${index}:`, html);
+
+          // Update items in the array
+          setData(prevData => {
+            const newData = [...prevData];
+            newData[index] = html;
+            return newData;
+          });
+        } catch (error) {
+          console.error(`Error on URL ${url}`, error);
+        }
+      });
     };
 
     fetchData();
@@ -26,7 +40,13 @@ const Listing = ({ items }) => {
   return (
     <ul>
       {data.map((item, index) => (
-        <li key={index} dangerouslySetInnerHTML={{ __html: item }}></li>
+        <li key={index}>
+          {item === null ? (
+            <span>Wird geladen...</span>
+          ) : (
+            <div dangerouslySetInnerHTML={{ __html: item }}></div>
+          )}
+        </li>
       ))}
     </ul>
   );
