f244762e20730798dd6eb7f5748dd2529f8aeacd
Philip Bauer <bauer@starzel.de>
2025-05-14 09:02:55 +0200

    Add upgrade to switch layout of ELANArchives (#5759)

--- backend/src/docpool.elan/docpool/elan/upgrades.py
+++ backend/src/docpool.elan/docpool/elan/upgrades.py
@@ -10,3 +10,7 @@ log = logging.getLogger(__name__)
 def to_1000(context=None):
     portal_setup = api.portal.get_tool("portal_setup")
     loadMigrationProfile(portal_setup, "profile-docpool.elan:to_1000")
+    for brain in api.content.find(portal_type="ELANArchives"):
+        obj = brain.getObject()
+        if obj.getLayout() != "view":
+            obj.setLayout("view")
