053ea7fb3d4e6806abd9985e1128095bda0099ca
Steffen Lindner <mail@steffen-lindner.de>
2025-06-04 11:57:05 +0200

    Add simple filter

--- backend/src/docpool.ui/docpool/ui/browser/listing.py
+++ backend/src/docpool.ui/docpool/ui/browser/listing.py
@@ -6,10 +6,17 @@ import json
 class Listing(BrowserView):
     """Example view called from template"""
 
-    def __call__(self):
+    def __call__(self, limit=0):
+        form = self.request.form
+        self.limit = int(form.get('limit', limit))
+
         # Do we fetch form catalog? UUIDs?
         dp_documents = api.content.find(context=self.context, portal_type="DPDocument")
         uids = [brain.UID for brain in dp_documents]
+
+        if self.limit > 0:
+            uids = uids[:self.limit]
+
         self.items = json.dumps(uids)
         return self.index()
 
