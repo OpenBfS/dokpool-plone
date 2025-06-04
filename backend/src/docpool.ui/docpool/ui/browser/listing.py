9ed29cd9f9c61a5a8a15908b76761e4a5c6743b6
Steffen Lindner <mail@steffen-lindner.de>
2025-06-04 12:07:18 +0200

    lint

--- backend/src/docpool.ui/docpool/ui/browser/listing.py
+++ backend/src/docpool.ui/docpool/ui/browser/listing.py
@@ -1,5 +1,6 @@
-from Products.Five.browser import BrowserView
 from plone import api
+from Products.Five.browser import BrowserView
+
 import json
 
 
@@ -8,14 +9,14 @@ class Listing(BrowserView):
 
     def __call__(self, limit=0):
         form = self.request.form
-        self.limit = int(form.get('limit', limit))
+        self.limit = int(form.get("limit", limit))
 
         # Do we fetch form catalog? UUIDs?
         dp_documents = api.content.find(context=self.context, portal_type="DPDocument")
         uids = [brain.UID for brain in dp_documents]
 
         if self.limit > 0:
-            uids = uids[:self.limit]
+            uids = uids[: self.limit]
 
         self.items = json.dumps(uids)
         return self.index()
@@ -27,6 +28,10 @@ class Item(BrowserView):
     def __call__(self, uid=None):
         self.uid = uid
         dpdocument_obj = api.content.get(UID=uid)
-        self.dpdocument = {"title": dpdocument_obj.title, "id": uid, "docType": dpdocument_obj.docType,
-                           "url": dpdocument_obj.absolute_url()}
+        self.dpdocument = {
+            "title": dpdocument_obj.title,
+            "id": uid,
+            "docType": dpdocument_obj.docType,
+            "url": dpdocument_obj.absolute_url(),
+        }
         return self.index()
