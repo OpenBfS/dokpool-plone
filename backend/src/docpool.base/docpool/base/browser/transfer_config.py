57b91d31184665e38f07f907d3720cfdbd644d5e
Thomas Lotze <lotze@starzel.de>
2025-07-24 12:05:14 +0200

    re #5233: make choice of adding or removing targets clearer

--- backend/src/docpool.base/docpool/base/browser/transfer_config.py
+++ backend/src/docpool.base/docpool/base/browser/transfer_config.py
@@ -9,8 +9,11 @@ from plone.z3cform import layout
 from z3c.form import button
 from z3c.form import form
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 
 import logging
 
@@ -19,6 +22,20 @@ logger = logging.getLogger(__name__)
 
 
 class ITransferConfig(model.Schema):
+    add = schema.Choice(
+        title=_(
+            "label_transfer_config_add_or_remove",
+            default="Automatic transfer for selected document types:",
+        ),
+        required=True,
+        default=True,
+        source=SimpleVocabulary([
+            SimpleTerm(True, title=_("label_transfer_config_add", default="add")),
+            SimpleTerm(False, title=_("label_transfer_config_remove", default="remove")),
+        ]),
+    )
+    directives.widget(add=RadioFieldWidget)
+
     types = schema.List(
         title=_("label_transfer_config_document_types", default="Document types"),
         description=_(
@@ -42,16 +59,6 @@ class ITransferConfig(model.Schema):
         value_type=schema.Choice(source="docpool.transfers.vocabularies.TransferTargets"),
     )
     directives.widget(targets=CheckBoxFieldWidget)
-
-    remove = schema.Bool(
-        title=_("label_transfer_config_remove", default="Remove selected targets?"),
-        description=_(
-            "desc_transfer_config_remove",
-            default="Whether to remove the selected targets for the selected types. Default is to add them.",
-        ),
-        required=False,
-        default=False,
-    )
 
 
 @implementer(IDexterityEditForm)
@@ -80,14 +87,14 @@ class TransferConfigView(AutoExtensibleForm, form.Form):
 
         types = [api.content.get(UID=t) for t in data["types"]]
         targets = data["targets"]
-        remove = data["remove"]
+        add = data["add"]
 
         for doctype in types:
             doctype_targets = set(ITransfersType(doctype).automaticTransferTargets)
-            if remove:
-                doctype_targets.difference_update(targets)
-            else:
+            if add:
                 doctype_targets.update(targets)
+            else:
+                doctype_targets.difference_update(targets)
             ITransfersType(doctype).automaticTransferTargets = list(doctype_targets)
 
         self.status = f"Configured {len(types)} types for {len(targets)} targets."
