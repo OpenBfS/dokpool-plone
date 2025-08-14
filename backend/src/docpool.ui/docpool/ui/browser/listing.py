b92ae9b0d9e474fba03f67b8357d340e864eaeef
Philip Bauer <bauer@starzel.de>
2025-08-14 14:19:23 +0200

    handle transfer-action, translate state and transitions

--- backend/src/docpool.ui/docpool/ui/browser/listing.py
+++ backend/src/docpool.ui/docpool/ui/browser/listing.py
@@ -1,10 +1,11 @@
-from docpool.base.utils import is_rei_workflow
+from docpool.base.behaviors.transferable import ITransferable
+from docpool.base.behaviors.utils import allowed_targets
+from docpool.base.utils import get_current_state_title
 from docpool.ui.services.newdatacheck.post import Listing as ListingBase
 from plone import api
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from Products.Five.browser import BrowserView
 from zope.component import queryUtility
-from zope.i18n import translate
 
 import json
 
@@ -25,23 +26,31 @@ class Item(BrowserView):
     """Example view called from template"""
 
     def __call__(self, uid=None):
-        request = self.request
         self.uid = uid
         obj = api.content.get(UID=uid)
 
-        portal_workflow = api.portal.get_tool("portal_workflow")
         review_state = api.content.get_state(obj)
-        state_title = portal_workflow.getTitleForStateOnType(review_state, obj.portal_type)
-        workflow_i18n_domain = "docpool.rei" if is_rei_workflow(obj) else "docpool.base"
-        translated_state_title = translate(state_title, domain=workflow_i18n_domain, context=request)
+        review_state_title = get_current_state_title(obj, review_state)
+
         idnormalizer = queryUtility(IIDNormalizer)
         state_class = f"state-{idnormalizer.normalize(review_state)}"
+        portal_workflow = api.portal.get_tool("portal_workflow")
         available_transitions = portal_workflow.getTransitionsFor(obj)
 
         if userinfo := obj.modified_by or obj.created_by:
             userinfo = userinfo.replace("<i>", "--separator--<i>", 1)
-            user = userinfo.split("--separator--")[0]
-            group = userinfo.split("--separator--")[1] if "--separator--" in userinfo else ""
+            modified_by_user = userinfo.split("--separator--")[0]
+            modified_by_group = userinfo.split("--separator--")[1] if "--separator--" in userinfo else ""
+
+        show_transfer_action = False
+        if api.user.has_permission("Docpool: Send Content", obj=obj):
+            try:
+                adapted = ITransferable(obj)
+            except TypeError:
+                pass
+            else:
+                if adapted.transferable() and allowed_targets(obj):
+                    show_transfer_action = True
 
         self.dpdocument = {
             "date": obj.mdate,
@@ -49,13 +58,15 @@ class Item(BrowserView):
             "id": obj.id,
             "description": obj.description,
             "review_state": review_state,
-            "state_title": translated_state_title,
+            "state_title": review_state_title,
             "state_class": state_class,
             "available_transitions": available_transitions,
             "uid": uid,
             "docType": obj.docType,
             "url": obj.absolute_url(),
-            "modified_by_user": user,
-            "modified_by_group": group,
+            "path": obj.absolute_url_path(),
+            "modified_by_user": modified_by_user,
+            "modified_by_group": modified_by_group,
+            "show_transfer_action": show_transfer_action,
         }
         return self.index()
