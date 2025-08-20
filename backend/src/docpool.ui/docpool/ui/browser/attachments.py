21da2e4ce3c26944536c05320e8a81bc22670bc0
Philip Bauer <bauer@starzel.de>
2025-08-20 17:46:19 +0200

    Start with attachments view

---
+++ backend/src/docpool.ui/docpool/ui/browser/attachments.py
@@ -0,0 +1,38 @@
+from plone.base.utils import human_readable_size
+from Products.Five.browser import BrowserView
+
+
+class Attachments(BrowserView):
+    def items(self):
+        contentlisting = self.context.restrictedTraverse("@@contentlisting")
+        query = {}
+        query["portal_type"] = [
+            "Image",
+            "File",
+        ]
+        results = [b for b in contentlisting(**query)]
+        return results
+
+    def total_size(self):
+        sizes = [i.getSize() for i in self.items()]
+        complete = complete_size_in_bytes(sizes)
+        return human_readable_size(complete)
+
+
+def complete_size_in_bytes(values):
+    """Calculate total sum of sizes ["1.2 KB", "2 MB"] as stored on brains."""
+    SIZE_CONST = {
+        "KB": 1024,
+        "MB": 1024**2,
+        "GB": 1024**3,
+        "TB": 1024**4,
+        "PB": 1024**5,
+    }
+    total_bytes = 0
+    for v in values:
+        if not v:
+            continue
+        num, unit = v.strip().split()
+        num = float(num.replace(",", "."))
+        total_bytes += num * SIZE_CONST[unit.upper()]
+    return total_bytes
