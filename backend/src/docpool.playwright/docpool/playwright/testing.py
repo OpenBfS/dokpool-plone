5820c54fb7203979010f99b4ab958ab1bb8499ef
Steffen Lindner <mail@steffen-lindner.de>
2025-08-27 16:37:25 +0200

    Adapt to docpool project. Add simple test

--- backend/src/docpool.playwright/docpool/playwright/testing.py
+++ backend/src/docpool.playwright/docpool/playwright/testing.py
@@ -1,34 +1,57 @@
 # -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import zope
-from plone.testing import z2
-
-import docpool.playwright
 
 
 class DocpoolPlaywrightLayer(PloneSandboxLayer):
-
-    defaultBases = (PLONE_FIXTURE,)
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         # Load any other ZCML that is required for your tests.
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
-        import plone.app.dexterity
-
-        self.loadZCML(package=plone.app.dexterity)
+        import collective.impersonate
+        import docpool.base
+        import docpool.elan
+        import docpool.playwright
+        import docpool.ui
+        import eea.facetednavigation
         import plone.restapi
 
         self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=docpool.base)
+        self.loadZCML(package=docpool.elan)
+        # self.loadZCML(package=docpool.ui)
+        self.loadZCML(package=eea.facetednavigation)
+        self.loadZCML(package=collective.impersonate)
         self.loadZCML(package=docpool.playwright)
 
     def setUpPloneSite(self, portal):
+        applyProfile(portal, "docpool.base:default")
+        applyProfile(portal, "docpool.elan:default")
+        applyProfile(portal, "elan.journal:default")
+        # applyProfile(portal, "docpool.ui:default")
         applyProfile(portal, "docpool.playwright:default")
+        # setRoles(portal, TEST_USER_ID, ["Manager"])
+        # # Create a docpool
+        # # Do it here because it takes a long time and creating a docpool
+        # # in each test or test-setup will lead to very long tests.
+        # dp = api.content.create(
+        #     container=portal,
+        #     type="DocumentPool",
+        #     id="bund",
+        #     title="Bund",
+        #     prefix="bund",
+        #     supportedApps=("elan",),
+        # )
+        # notify(EditFinishedEvent(dp))
+        # portal.acl_users.userFolderAddUser(SITE_OWNER_NAME, SITE_OWNER_PASSWORD, ["Manager"], [])
+        #
 
 
 DOCPOOL_PLAYWRIGHT_FIXTURE = DocpoolPlaywrightLayer()
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,46 +1,32 @@
-from _pytest.pytester import Pytester
-from _pytest.python import Metafunc
-import base64
-from pathlib import Path
-from typing import Generator
-
-import pytest
-import transaction
+from docpool.playwright.testing import DOCPOOL_PLAYWRIGHT_ACCEPTANCE_TESTING
+from docpool.playwright.testing import DOCPOOL_PLAYWRIGHT_FUNCTIONAL_TESTING
+from docpool.playwright.testing import DOCPOOL_PLAYWRIGHT_INTEGRATION_TESTING
 from playwright.sync_api import Page
 from plone import api
-from plone.app.testing.interfaces import (
-    SITE_OWNER_NAME,
-    SITE_OWNER_PASSWORD,
-    TEST_USER_NAME,
-)
-from plone.testing.zope import WSGI_SERVER_FIXTURE
+from plone.app.testing.interfaces import SITE_OWNER_NAME
+from plone.app.testing.interfaces import SITE_OWNER_PASSWORD
+from plone.app.testing.interfaces import TEST_USER_NAME
 from pytest_plone import fixtures_factory
-from pythongettext.msgfmt import Msgfmt, PoSyntaxError
 from zope.component.hooks import setSite
 
-from docpool.playwright.testing import (
-    DOCPOOL_PLAYWRIGHT_INTEGRATION_TESTING,
-    DOCPOOL_PLAYWRIGHT_FUNCTIONAL_TESTING,
-    DOCPOOL_PLAYWRIGHT_ACCEPTANCE_TESTING
-)
+import base64
+import pytest
+import transaction
+
 
 pytest_plugins = ["pytest_plone"]
 
 globals().update(
-    fixtures_factory(
-        (
-            (DOCPOOL_PLAYWRIGHT_ACCEPTANCE_TESTING, "acceptance"),
-            (DOCPOOL_PLAYWRIGHT_FUNCTIONAL_TESTING, "functional"),
-            (DOCPOOL_PLAYWRIGHT_INTEGRATION_TESTING, "integration"),
-        )
-    )
+    fixtures_factory((
+        (DOCPOOL_PLAYWRIGHT_ACCEPTANCE_TESTING, "acceptance"),
+        (DOCPOOL_PLAYWRIGHT_FUNCTIONAL_TESTING, "functional"),
+        (DOCPOOL_PLAYWRIGHT_INTEGRATION_TESTING, "integration"),
+    ))
 )
 
 
 def generate_basic_authentication_header_value(username: str, password: str) -> str:
-    token = base64.b64encode("{}:{}".format(username, password).encode("utf-8")).decode(
-        "ascii"
-    )
+    token = base64.b64encode("{}:{}".format(username, password).encode("utf-8")).decode("ascii")
     return "Basic {}".format(token)
 
 
@@ -48,7 +34,7 @@ def generate_basic_authentication_header_value(username: str, password: str) -> 
 def portal_factory(acceptance, request):
     def factory(roles: list, username: str = TEST_USER_NAME):
         if not roles:
-            roles = ['Member']
+            roles = ["Member"]
         portal = acceptance["portal"]
         setSite(portal)
         with api.env.adopt_roles(["Manager", "Member"]):
@@ -74,14 +60,10 @@ def portal_factory(acceptance, request):
 
 @pytest.fixture()
 def playwright_page_factory(new_context):
-    def factory(
-        username: str = SITE_OWNER_NAME, password: str = SITE_OWNER_PASSWORD
-    ) -> Page:
+    def factory(username: str = SITE_OWNER_NAME, password: str = SITE_OWNER_PASSWORD) -> Page:
         context = new_context(
             extra_http_headers={
-                "Authorization": generate_basic_authentication_header_value(
-                    username, password
-                ),
+                "Authorization": generate_basic_authentication_header_value(username, password),
             }
         )
         page = context.new_page()
