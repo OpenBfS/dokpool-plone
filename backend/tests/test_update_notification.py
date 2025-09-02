51cb91ed02aee2abcbe88d731ff3ae90d09a4c20
Steffen Lindner <mail@steffen-lindner.de>
2025-09-02 16:10:34 +0200

    Add simple listing test

---
+++ backend/tests/test_update_notification.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import expect
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_NAME
+
+import pytest
+
+
+class TestUpdateNotification:
+    @pytest.fixture(autouse=True)
+    def setup(self, portal_factory, playwright_page_factory) -> None:
+        self.portal = portal_factory(username=TEST_USER_NAME, roles=["Manager"])
+        self.page = playwright_page_factory(username=SITE_OWNER_NAME, password=SITE_OWNER_PASSWORD)
+        self.plone_url = self.portal.absolute_url()
+
+    def test_listing(self):
+        page = self.page
+        page.goto(f"{self.plone_url}/bund/listing")
+        # Tests if the DPDocument exists
+        first_list_item = page.locator(".listing-item h2").first
+        expect(first_list_item).to_have_text("Weatherinfo")
+        page.pause()
