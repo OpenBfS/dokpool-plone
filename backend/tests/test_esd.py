baefb251c1c73c03929e108dd2a75dd2f2349624
Steffen Lindner <mail@steffen-lindner.de>
2025-09-02 13:28:42 +0200

    Make it a real test and better naming

--- backend/tests/test_esd.py
+++ backend/tests/test_esd.py
@@ -6,14 +6,15 @@ from plone.app.testing import TEST_USER_NAME
 import pytest
 
 
-class TestPwNews:
+class TestEsd:
     @pytest.fixture(autouse=True)
     def setup(self, portal_factory, playwright_page_factory) -> None:
         self.portal = portal_factory(username=TEST_USER_NAME, roles=["Manager"])
         self.page = playwright_page_factory(username=SITE_OWNER_NAME, password=SITE_OWNER_PASSWORD)
         self.plone_url = self.portal.absolute_url()
 
-    def test_news_listing(self) -> None:
+    def test_bund_esd_page(self) -> None:
         page = self.page
         page.goto(f"{self.plone_url}/bund/esd")
-        expect(page.get_by_role("heading", name="Digitale Lagedarstellung"))
+        first_h1 = page.locator("h1").first
+        expect(first_h1).to_have_text("Elektronische Lagedarstellung")
