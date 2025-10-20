4be3e6b9dd90080a0dd9be6aa8ecd980b5e66e4b
Philip Bauer <bauer@starzel.de>
2025-10-20 18:51:51 +0200

    use modal from pat-patternslib

--- backend/src/docpool.ui/docpool/ui/resources/default.js
+++ backend/src/docpool.ui/docpool/ui/resources/default.js
@@ -1,5 +1,7 @@
+import registry from "@patternslib/patternslib/src/core/registry";
 import "bootstrap";
 import "./docpool.scss";
 // Pat-update-notification
 import("./pat-update-notification/index.js");
-
+import("@patternslib/patternslib/src/pat/modal/modal");
+registry.init();
