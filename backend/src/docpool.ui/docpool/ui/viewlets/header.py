162ec27af9ae4c6c6f747732a5a6c818941a1031
Steffen Lindner <mail@steffen-lindner.de>
2025-10-23 17:13:05 +0200

    Add some dropdowns

--- backend/src/docpool.ui/docpool/ui/viewlets/header.py
+++ backend/src/docpool.ui/docpool/ui/viewlets/header.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from Products.Five.browser import BrowserView
 
 
 class PortalHeader(ViewletBase):
+    pass
+
+
+class InfoDropdown(BrowserView):
+    pass
+
+
+class UserDropdown(BrowserView):
+    pass
+
+
+class ConfigDropdown(BrowserView):
     pass
