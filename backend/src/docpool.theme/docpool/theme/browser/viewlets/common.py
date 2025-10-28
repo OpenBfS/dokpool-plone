09a9b594c00be530177ba1adec52bcccbb437a47
Steffen Lindner <mail@steffen-lindner.de>
2025-10-28 11:21:21 +0100

    Move version into new header

--- backend/src/docpool.theme/docpool/theme/browser/viewlets/common.py
+++ backend/src/docpool.theme/docpool/theme/browser/viewlets/common.py
@@ -1,5 +1,4 @@
 from AccessControl.SecurityInfo import allow_class
-from App.config import getConfiguration
 from docpool.base.appregistry import APP_REGISTRY
 from logging import getLogger
 from plone import api
@@ -8,9 +7,6 @@ from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 import datetime
-import os
-import shlex
-import subprocess
 
 
 logger = getLogger(__name__)
@@ -40,79 +36,3 @@ class LogoDocpoolViewlet(LogoViewlet):
         if not active_app:
             return {}
         return APP_REGISTRY[active_app[0]]
-
-    def available(self):
-        if api.user.is_anonymous():
-            return False
-        if api.portal.get_registry_record(name="docpool.show_debug_info"):
-            return True
-
-    def read_git_version_file(self, filename):
-        read_file = False
-        file_path = ""
-        # Try to get the path from the pid file :)
-        try:
-            varbase = os.path.dirname(getConfiguration().pid_filename)
-            project_root = os.path.abspath(os.path.join(varbase, ".."))
-            file_path = os.path.join(project_root, filename)
-        except AttributeError:
-            # Ignore if we have no access to file
-            pass
-
-        if os.path.isfile(file_path):
-            read_file = True
-
-        # New Plone backend docker structure
-        if os.path.isfile("/app/GIT_COMMIT"):
-            file_path = os.path.join("/app/GIT_COMMIT")
-            read_file = True
-
-        if not read_file:
-            return
-
-        with open(file_path) as file:
-            content = file.read()
-            if content:
-                return content
-
-    def get_git_rev(self):
-        # Git Revision
-
-        # If run inside docker with existing version file
-        version_in_file = self.read_git_version_file("GIT_COMMIT")
-        if version_in_file:
-            return version_in_file
-
-        # If run on the review server
-        commit_hash = os.getenv("GIT_COMMIT")
-        if commit_hash:
-            return commit_hash
-
-        # If run local
-        result = subprocess.run(
-            shlex.split("git rev-parse --short HEAD"),
-            capture_output=True,
-            text=True,
-            check=False,
-        )
-        if result.returncode == 128:
-            return "Not detected"
-
-        return result.stdout.strip()
-
-    def get_git_branch(self):
-        # Git Branch
-        commit_name = os.getenv("GIT_REF_NAME")
-        if commit_name:
-            return commit_name
-
-        result = subprocess.run(
-            shlex.split("git rev-parse --abbrev-ref HEAD"),
-            capture_output=True,
-            text=True,
-            check=False,
-        )
-        if result.returncode == 128:
-            return "Not detected"
-
-        return result.stdout.strip()
--- backend/src/docpool.ui/docpool/ui/viewlets/header.py
+++ backend/src/docpool.ui/docpool/ui/viewlets/header.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-
-from plone.app.layout.viewlets import ViewletBase
+from App.config import getConfiguration
+from importlib.metadata import distribution
+from plone import api
+from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser import BrowserView
+
+import os
+import shlex
+import subprocess
 
 
 class PortalHeader(ViewletBase):
@@ -9,7 +14,85 @@ class PortalHeader(ViewletBase):
 
 
 class InfoDropdown(BrowserView):
-    pass
+    def get_dokpool_version(self):
+        dist = distribution("docpool.base")
+        return dist.version
+
+    def available(self):
+        if api.user.is_anonymous():
+            return False
+        if api.portal.get_registry_record(name="docpool.show_debug_info"):
+            return True
+
+    def read_git_version_file(self, filename):
+        read_file = False
+        file_path = ""
+        # Try to get the path from the pid file :)
+        try:
+            varbase = os.path.dirname(getConfiguration().pid_filename)
+            project_root = os.path.abspath(os.path.join(varbase, ".."))
+            file_path = os.path.join(project_root, filename)
+        except AttributeError:
+            # Ignore if we have no access to file
+            pass
+
+        if os.path.isfile(file_path):
+            read_file = True
+
+        # New Plone backend docker structure
+        if os.path.isfile("/app/GIT_COMMIT"):
+            file_path = os.path.join("/app/GIT_COMMIT")
+            read_file = True
+
+        if not read_file:
+            return
+
+        with open(file_path) as file:
+            content = file.read()
+            if content:
+                return content
+
+    def get_git_rev(self):
+        # Git Revision
+
+        # If run inside docker with existing version file
+        version_in_file = self.read_git_version_file("GIT_COMMIT")
+        if version_in_file:
+            return version_in_file
+
+        # If run on the review server
+        commit_hash = os.getenv("GIT_COMMIT")
+        if commit_hash:
+            return commit_hash
+
+        # If run local
+        result = subprocess.run(
+            shlex.split("git rev-parse --short HEAD"),
+            capture_output=True,
+            text=True,
+            check=False,
+        )
+        if result.returncode == 128:
+            return "Not detected"
+
+        return result.stdout.strip()
+
+    def get_git_branch(self):
+        # Git Branch
+        commit_name = os.getenv("GIT_REF_NAME")
+        if commit_name:
+            return commit_name
+
+        result = subprocess.run(
+            shlex.split("git rev-parse --abbrev-ref HEAD"),
+            capture_output=True,
+            text=True,
+            check=False,
+        )
+        if result.returncode == 128:
+            return "Not detected"
+
+        return result.stdout.strip()
 
 
 class UserDropdown(BrowserView):
