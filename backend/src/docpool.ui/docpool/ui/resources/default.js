c2a6b0c138e0a499ea1815345f1a3ebf408671cc
Philip Bauer <bauer@starzel.de>
2025-10-29 13:40:51 +0100

    depend on plone.patternslib instead of adding individual patterns

--- backend/src/docpool.ui/docpool/ui/resources/default.js
+++ backend/src/docpool.ui/docpool/ui/resources/default.js
@@ -1,8 +1,4 @@
-import registry from "@patternslib/patternslib/src/core/registry";
 import "bootstrap";
 import "./docpool.scss";
 // Pat-update-notification
 import("./pat-update-notification/index.js");
-import("@patternslib/patternslib/src/pat/modal/modal");
-import("@patternslib/patternslib/src/pat/auto-submit/auto-submit");
-registry.init();
