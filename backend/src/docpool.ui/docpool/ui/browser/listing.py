86574a3bf4fc5350dec7ae187d1cac10ab268286
Philip Bauer <bauer@starzel.de>
2025-11-05 09:27:27 +0100

    move find into Listing

--- backend/src/docpool.ui/docpool/ui/browser/listing.py
+++ backend/src/docpool.ui/docpool/ui/browser/listing.py
@@ -1,8 +1,12 @@
 from docpool.base.behaviors.transferable import ITransferable
 from docpool.base.behaviors.utils import allowed_targets
+from docpool.base.config import BASE_APP
+from docpool.base.config import TRANSFERS_APP
+from docpool.base.content.archiving import IArchiving
 from docpool.base.content.dpdocument import IDPDocument
 from docpool.base.utils import get_current_state_title
-from docpool.ui.services.newdatacheck.post import Listing as ListingBase
+from docpool.elan.config import ELAN_APP
+from docpool.elan.utils import getScenariosForCurrentUser
 from plone import api
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from Products.Five.browser import BrowserView
@@ -31,7 +35,7 @@ TRANSITION_ICON_MAPPING = {
 }
 
 
-class Listing(ListingBase, BrowserView):
+class Listing(BrowserView):
     """Example view called from template"""
 
     def __call__(self, limit=0):
@@ -41,6 +45,48 @@ class Listing(ListingBase, BrowserView):
         self.items = uids
         self.modified = json.dumps(modified.timeTime()) if modified else None
         return self.index()
+
+    def find(self, limit=0):
+        form = self.request.form
+        self.limit = int(form.get("limit", limit))
+
+        self.selected_doktypes = form.get("selected_doktypes") or []
+        self.documenttypes_vocabulary = api.portal.get_vocabulary(
+            "docpool.base.vocabularies.DocumentTypes", self.context
+        )
+
+        query = {
+            "context": self.context,
+            "portal_type": ["DPDocument"],
+            "sort_on": "mdate",
+            "sort_order": "reverse",
+        }
+
+        # Filter by APP
+        dp_app_state = api.content.get_view("dp_app_state", self.context, self.request)
+        active_apps = dp_app_state.appsActivatedByCurrentUser()
+        active_apps.extend([BASE_APP, TRANSFERS_APP])
+        query["apps_supported"] = active_apps
+
+        # Filter by DPEvent (ELAN only)
+        if ELAN_APP in active_apps and not IArchiving(self.context).is_archive:
+            if event := getScenariosForCurrentUser():
+                query["scenarios"] = event
+
+        # Manual filtering
+        if self.limit:
+            query["sort_limit"] = self.limit
+        if self.selected_doktypes:
+            query["dp_type"] = self.selected_doktypes
+
+        brains = api.content.find(**query)
+        uids = [brain.UID for brain in brains]
+        modified = max(brain.modified for brain in brains) if brains else None
+
+        if self.limit > 0:
+            uids = uids[: self.limit]
+
+        return uids, modified
 
 
 class Item(BrowserView):
--- backend/src/docpool.ui/docpool/ui/services/newdatacheck/post.py
+++ backend/src/docpool.ui/docpool/ui/services/newdatacheck/post.py
@@ -1,57 +1,9 @@
-from docpool.base.config import BASE_APP
-from docpool.base.config import TRANSFERS_APP
-from docpool.base.content.archiving import IArchiving
-from docpool.elan.config import ELAN_APP
-from docpool.elan.utils import getScenariosForCurrentUser
 from plone import api
 from plone.restapi.services import Service
 
 
-class Listing:
-    def find(self, limit=0):
-        form = self.request.form
-        self.limit = int(form.get("limit", limit))
-
-        self.selected_doktypes = form.get("selected_doktypes") or []
-        self.documenttypes_vocabulary = api.portal.get_vocabulary(
-            "docpool.base.vocabularies.DocumentTypes", self.context
-        )
-
-        query = {
-            "context": self.context,
-            "portal_type": ["DPDocument"],
-            "sort_on": "mdate",
-            "sort_order": "reverse",
-        }
-
-        # Filter by APP
-        dp_app_state = api.content.get_view("dp_app_state", self.context, self.request)
-        active_apps = dp_app_state.appsActivatedByCurrentUser()
-        active_apps.extend([BASE_APP, TRANSFERS_APP])
-        query["apps_supported"] = active_apps
-
-        # Filter by DPEvent (ELAN only)
-        if ELAN_APP in active_apps and not IArchiving(self.context).is_archive:
-            if event := getScenariosForCurrentUser():
-                query["scenarios"] = event
-
-        # Manual filtering
-        if self.limit:
-            query["sort_limit"] = self.limit
-        if self.selected_doktypes:
-            query["dp_type"] = self.selected_doktypes
-
-        brains = api.content.find(**query)
-        uids = [brain.UID for brain in brains]
-        modified = max(brain.modified for brain in brains) if brains else None
-
-        if self.limit > 0:
-            uids = uids[: self.limit]
-
-        return uids, modified
-
-
-class NewDataCheck(Listing, Service):
+class NewDataCheck(Service):
     def reply(self):
-        _, modified = self.find()
+        listing = api.content.get_view("listing", self.context, self.request)
+        _, modified = listing.find(limit=0)
         return {"modified_last": (modified.timeTime() if modified else None)}
