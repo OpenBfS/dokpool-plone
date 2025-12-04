07f3439b479f5297ba83522d8ff9351a457e759c
Thomas Lotze <lotze@starzel.de>
2025-12-04 09:43:43 +0100

    re #6338: remove transfer veto by ELAN because of unknown scenarios
    Also closes #5723 by removing the code in question.

--- backend/src/docpool.elan/docpool/elan/content/transfers.py
+++ backend/src/docpool.elan/docpool/elan/content/transfers.py
@@ -1,11 +1,9 @@
-from docpool.base import DocpoolMessageFactory as _
 from docpool.base.behaviors.transferable import IAppSpecificTransfer
 from docpool.base.content.dpdocument import IDPDocument
 from docpool.base.content.dptransferfolder import IDPTransferFolder
 from docpool.base.utils import _copyPaste
 from docpool.elan.behaviors.elandocument import IELANDocument
 from docpool.elan.config import ELAN_APP
-from docpool.elan.utils import getOpenScenarios
 from plone import api
 from Products.CMFPlone.utils import log_exc
 from zope.component import adapter
@@ -24,28 +22,7 @@ class ELANSpecificTransfer:
         self.have_elan = ELAN_APP in self.transfer_folder.myDocumentPool().supportedApps
 
     def assert_allowed(self):
-        if not self.have_elan:
-            return
-
-        # Scenarios unknown at the target may either be blocked or handled (which currently means they will be
-        # created at the target later on). If they are going to be handled anyway, we're fine.
-        if self.transfer_folder.unknownScenDefault != "block":
-            return
-
-        # At this point, we're not going to be able to keep scenarios that are unknown at the target. Our
-        # policy is to veto against the transfer rather than lose scenario associations.
-
-        # TODO The following is inefficient in that it creates a list of
-        # full objects, but it effectively filters elanobj.scenarios for
-        # those that actually exist in the catalog. Is this necessary?
-        # FIXME: The following logic appears to be broken, see #5723.
-        scens = self.elanobj.myScenarioObjects()
-        if scens:
-            scen_id = scens[0].getId()
-            if not any(scen.getId == scen_id for scen in getOpenScenarios(self.transfer_folder)):
-                raise ValueError(_("Unknown scenario not accepted."))
-        else:
-            raise ValueError(_("Document has no scenario."))
+        return
 
     def sender_log_entry(self):
         scenario_ids = ", ".join(b.getId for b in api.content.find(UID=self.elanobj.scenarios))
