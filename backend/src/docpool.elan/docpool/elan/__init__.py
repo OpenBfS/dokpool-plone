44c2efc02f5c36d48117493a3a8082491e7f6b40
Philip Bauer <bauer@starzel.de>
2025-12-17 11:12:40 +0100

    lint

--- backend/src/docpool.elan/docpool/elan/__init__.py
+++ backend/src/docpool.elan/docpool/elan/__init__.py
@@ -5,6 +5,8 @@ from zope.i18nmessageid import MessageFactory
 DocpoolMessageFactory = MessageFactory("docpool.elan")
 
 from docpool.elan import appregistration  # noqa: F401
+
+
 # TODO: Remove once the new GUI is finished
 # from docpool.elan import monkey  # noqa: F401
 
