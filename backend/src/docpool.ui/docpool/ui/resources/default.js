b589d1028625bfe0d8369190c863617389900789
Steffen Lindner <mail@steffen-lindner.de>
2026-01-22 10:16:47 +0100

    Add event listener comment to clarify functionality

--- backend/src/docpool.ui/docpool/ui/resources/default.js
+++ backend/src/docpool.ui/docpool/ui/resources/default.js
@@ -4,6 +4,7 @@ import "./docpool.scss";
 import("./pat-update-notification/index.js");
 import registry from "@patternslib/patternslib/src/core/registry";
 
+// After inject this code sets the back link to listing and populates next/prev links
 document.addEventListener("patterns-injected-delayed", (e) => {
   if (!(e.target instanceof Element)) return;
 
