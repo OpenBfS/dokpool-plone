9d0fd2a006b6af168636ef223549b2c52fff929a
Thomas Lotze <lotze@starzel.de>
2026-02-06 12:04:12 +0100

    re #6342: add guard expression to retracting published documents
    This expression allows apps to veto against retracting by looking for
    registered app-specific components.

--- backend/src/docpool.base/docpool/base/content/dpdocument.py
+++ backend/src/docpool.base/docpool/base/content/dpdocument.py
@@ -41,6 +41,7 @@ from zope import schema
 from zope.annotation.interfaces import IAnnotations
 from zope.component import adapter
 from zope.component import getUtilitiesFor
+from zope.component import queryAdapter
 from zope.component import queryMultiAdapter
 from zope.container.interfaces import IContainerModifiedEvent
 from zope.globalrequest import getRequest
@@ -63,6 +64,11 @@ def default_text():
     request = getRequest()
     if request and "reireport" in request.get("form.widgets.docType", []):
         return RichTextValue("REI-Bericht", "text/html", "text/x-html-safe")
+
+
+class IAppSpecificWorkflow(Interface):
+    def is_transition_allowed(transition):
+        pass
 
 
 class IDPDocument(IContentBase):
@@ -544,6 +550,13 @@ class DPDocument(Container, Extendable, ContentBase):
             return False
         doc_type = obj.docTypeObj()
         return doc_type.allow_discussion_on_dpdocument if doc_type else False
+
+    def is_transition_allowed(self, transition):
+        for app in ILocalBehaviorSupport(self).local_behaviors:
+            app_wf = queryAdapter(self, IAppSpecificWorkflow, name=app)
+            if app_wf and not app_wf.is_transition_allowed(transition):
+                return False
+        return True
 
 
 @adapter(IDPDocument, IContainerModifiedEvent)
