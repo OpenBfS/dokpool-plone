415aed568ff030d59a11d183bf988e24feced618
Philip Bauer <bauer@starzel.de>
2026-02-06 16:26:10 +0100

    start removing some old types

--- backend/src/docpool.elan/docpool/elan/content/elancurrentsituation.py
+++ backend/src/docpool.elan/docpool/elan/content/elancurrentsituation.py
@@ -1,25 +1,5 @@
-from docpool.elan.config import ELAN_APP
 from plone.dexterity.content import Container
-from plone.supermodel import model
-from zope.interface import implementer
 
 
-class IELANCurrentSituation(model.Schema):
-    pass
-
-
-@implementer(IELANCurrentSituation)
 class ELANCurrentSituation(Container):
-    APP = ELAN_APP
-
-    def correctAllDocTypes(self):
-        # Correct references
-        mpath = self.dpSearchPath()
-        from docpool.base.utils import queryForObjects
-
-        ecs = queryForObjects(self, path=mpath, portal_type="ELANDocCollection")
-        for ec in ecs:
-            ec.getObject().correctDocTypes()
-
-    def myELANCurrentSituation(self):
-        return self
+    pass
