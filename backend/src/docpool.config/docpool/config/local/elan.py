0f05134eb8b9ac63eb9943262221848e86a12d1b
Philip Bauer <bauer@starzel.de>
2026-02-13 12:59:57 +0100

    Rename Dokumentensammlung to Kategorie

--- backend/src/docpool.config/docpool/config/local/elan.py
+++ backend/src/docpool.config/docpool/config/local/elan.py
@@ -109,7 +109,7 @@ ADMINSTRUCTURE = [
             {TYPE: "Text", TITLE: "Ticker", ID: "ticker", CHILDREN: []},
             {
                 TYPE: "DashboardsConfig",
-                TITLE: "Eintragsammlungen Pinnwand",
+                TITLE: "Kategorien Pinnwand",
                 ID: "dbconfig",
                 CHILDREN: [],
             },
--- backend/src/docpool.config/docpool/config/local/elan_de.py
+++ backend/src/docpool.config/docpool/config/local/elan_de.py
@@ -107,7 +107,7 @@ ADMINSTRUCTURE = [
             {TYPE: "Text", TITLE: "Hilfe", ID: "help", CHILDREN: []},
             {
                 TYPE: "DashboardsConfig",
-                TITLE: "Eintragsammlungen Pinnwand",
+                TITLE: "Kategorien Pinnwand",
                 ID: "dbconfig",
                 CHILDREN: [],
             },
