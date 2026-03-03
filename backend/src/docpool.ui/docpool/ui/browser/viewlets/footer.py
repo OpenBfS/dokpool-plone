9591011618d3e74fb59aa96688263072a9f8a906
Steffen Lindner <mail@steffen-lindner.de>
2026-03-03 13:49:49 +0100

    Move and style timezone box

--- backend/src/docpool.ui/docpool/ui/browser/viewlets/footer.py
+++ backend/src/docpool.ui/docpool/ui/browser/viewlets/footer.py
@@ -1,5 +1,14 @@
 from plone.app.layout.viewlets.common import ViewletBase
 
+import datetime
+
 
 class PortalFooter(ViewletBase):
-    pass
+    def get_local_time(self):
+        return datetime.datetime.now()
+
+    def get_utc_time(self):
+        return datetime.datetime.now(datetime.UTC)
+
+    def get_jst_time(self):
+        return datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=8)
