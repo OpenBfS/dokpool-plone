14368898e97250bf52eb0ba87d249fd06357ce4e
Thomas Lotze <lotze@starzel.de>
2026-03-11 15:32:10 +0100

    re #6446: implement IPlaces API with document_pool property

---
+++ backend/src/docpool.base/docpool/base/content/places.py
@@ -0,0 +1,29 @@
+from zope.component import adapter
+from zope.interface import Attribute
+from zope.interface import implementer
+from zope.interface import Interface
+from zope.location.interfaces import ILocation
+
+
+class IPlaces(Interface):
+    """
+    API for accessing well-known places in the content hierarchy w.r.t. the context object.
+
+    The context may be None.
+    """
+
+    document_pool = Attribute("Document pool")
+
+
+@adapter(Interface)
+@implementer(IPlaces)
+class PlacesAPI:
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def document_pool(self):
+        obj = self.context
+        while obj is not None and obj.portal_type != "DocumentPool":
+            obj = ILocation(obj).__parent__
+        return obj
