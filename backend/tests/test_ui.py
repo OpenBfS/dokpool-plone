2b1ef72b1d069cbb81bc88e8b9ac5817236fd7ee
Steffen Lindner <mail@steffen-lindner.de>
2026-04-08 13:44:45 +0200

    Add test for desktop navigation event switch

--- backend/tests/test_ui.py
+++ backend/tests/test_ui.py
@@ -19,3 +19,24 @@ class TestUI:
         page.get_by_role("button", name="Info").click()
         info_dropdown = page.locator("#docpool-header .desktop-settings-infos .menu-info-dropdown")
         expect(info_dropdown.get_by_text("Softwareversion")).to_be_visible()
+
+    def test_desktop_nav_switch_event(self):
+        page = self.page
+        page.goto(f"{self.plone_url}/bund/setActiveApp?app=elan")
+
+        expect(page.locator("#docpool-header .desktop-settings-infos")).to_be_visible()
+        selected_event_title = page.locator(
+            "#docpool-header .event-card.event-switcher-detail .text-truncate",
+        ).first
+        expect(selected_event_title).to_have_text("Normalfall")
+
+        event_switcher = page.locator("#docpool-header .event-switcher").first
+        expect(event_switcher).to_be_visible()
+        event_switcher.click()
+
+        notfall_link = page.locator("#event-dropdown-menu a", has_text="Notfall").first
+        expect(notfall_link).to_be_visible()
+        notfall_link.click()
+        page.wait_for_load_state("networkidle")
+
+        expect(selected_event_title).to_have_text("Notfall")
