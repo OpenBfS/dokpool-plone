08ea31d2f8851efe644cf7f18f87cf0023429ee4
Philip Bauer <bauer@starzel.de>
2026-04-21 10:10:35 +0200

    usergroup-userprefs: Do not fail on user without principal object (test-bund-fr) and speed up listing

--- backend/src/docpool.base/docpool/base/users/search.py
+++ backend/src/docpool.base/docpool/base/users/search.py
@@ -8,11 +8,19 @@ class PASSearchView(PSV):
     def searchUsers(self, sort_by=None, **criteria):
         alsoProvides(self.context.REQUEST, IDisableCSRFProtection)
         self.pas = getToolByName(self.context, "acl_users")
-        mtool = getToolByName(self, "portal_membership")
         results = self.merge(self.pas.searchUsers(**criteria), "userid")
         if sort_by is not None:
             results = self.sort(results, sort_by)
-        return [r for r in results if mtool.getMemberById(r["id"]).getProperty("dp") == self.context.UID()]
+        filtered_results = []
+        if results:
+            uid = self.context.UID()
+            for data in results:
+                # pas.getUserById is 2x faster than portal_membership.getMemberById and finds the same!
+                user = self.pas.getUserById(data["id"])
+                # Also ignore users without principal object!
+                if user and user.getProperty("dp") == uid:
+                    filtered_results.append(data)
+        return filtered_results
 
     def searchGroups(self, sort_by=None, **criteria):
         alsoProvides(self.context.REQUEST, IDisableCSRFProtection)
